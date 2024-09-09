d20e581cea120805b43627626538596dfcc5ad5b
JiEung2 <wldmd5007@naver.com>
2024-09-09 17:39:14 +0900

    feat: Refresh 토큰 엔티티 생성

---
+++ project/back/health/src/main/java/com/ssafy/health/common/oauth/entity/RefreshToken.java
@@ -0,0 +1,21 @@
+package com.ssafy.health.common.oauth.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String refresh;
+    private String expiration;
+}
