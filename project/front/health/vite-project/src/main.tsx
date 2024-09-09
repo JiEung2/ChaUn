c0aea6d9cfe2c5e804f30c7e8c9b263b9805c7f4
인호현 <inhohyun36@gmail.com>
2024-09-09 14:13:14 +0900

    create frontend

---
+++ project/front/health/vite-project/src/main.tsx
@@ -0,0 +1,19 @@
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(<App />);
+
+// 서비스 워커 등록 코드 추가
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('서비스 워커가 다음과 같은 scope에서 등록되었습니다: ', registration.scope);
+      })
+      .catch((error) => {
+        console.log('서비스워커 등록에 실패했습니다: ', error);
+      });
+  });
+}
