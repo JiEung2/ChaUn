24fce3b223a9785336c406d335905f5d79f4887a
인호현 <inhohyun36@gmail.com>
2024-09-09 17:57:15 +0900

    chore: 폴더구조 설정

--- project/front/health/src/App.tsx
+++ project/front/health/src/App.tsx
@@ -1,9 +1,5 @@
 const App = () => {
-  return (
-    <div className="container">
-      <h1 className="title">App.tsx</h1>
-    </div>
-  );
+  return <div className="container"></div>;
 };
 
 export default App;
