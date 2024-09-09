141444e618d737c1bd6ea08618adf7350ba6add3
JiEung2 <wldmd5007@naver.com>
2024-09-09 17:33:16 +0900

    feat: CustomOAuth2User 생성

---
+++ project/back/health/src/main/java/com/ssafy/health/common/oauth/dto/CustomOAuth2User.java
@@ -0,0 +1,49 @@
+package com.ssafy.health.common.oauth.dto;
+
+import com.ssafy.health.domain.account.dto.response.UserRegisterResponseDto;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOAuth2User implements OAuth2User {
+
+    private final UserRegisterResponseDto userRegisterResponseDto;
+
+    public CustomOAuth2User(UserRegisterResponseDto userRegisterResponseDto) {
+        this.userRegisterResponseDto = userRegisterResponseDto;
+    }
+
+
+    @Override
+    public Map<String, Object> getAttributes() {
+
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return userRegisterResponseDto.getRole().name();
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getName() {
+        return userRegisterResponseDto.getName();
+    }
+
+    public String getUsername() {
+        return userRegisterResponseDto.getSso();
+    }
+}
