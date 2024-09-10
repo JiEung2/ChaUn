97486e75a933fc042250064c26e7b028920d5cd2
JiEung2 <wldmd5007@naver.com>
2024-09-10 10:10:53 +0900

    feat: UserExceptionMessage 추가
    - user nickname duplicate 메시지 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/account/exception/UserExceptionMessage.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/account/exception/UserExceptionMessage.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum UserExceptionMessage {
-    USER_NOT_FOUND("해당 유저가 존재하지 않습니다.", HttpStatus.NOT_FOUND.value()),;
+    USER_NOT_FOUND("해당 유저가 존재하지 않습니다.", HttpStatus.NOT_FOUND.value()),
+    USER_NICKNAME_DUPLICATED("이미 존재하는 닉네임입니다.", HttpStatus.CONFLICT.value()),
+    ;
 
     private final String message;
     private final int status;
