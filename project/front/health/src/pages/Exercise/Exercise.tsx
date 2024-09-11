7b9d0794c5904984d3aebecf5e912f3da29d46cb
minyeong981 <rlaalsdud41@naver.com>
2024-09-11 13:21:32 +0900

    feat: exercise modal 추가

--- project/front/health/src/pages/Exercise/Exercise.tsx
+++ project/front/health/src/pages/Exercise/Exercise.tsx
@@ -1,13 +1,18 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import ExerciseModal from '../../components/Exercise/ExerciseModal';
+import Modal from 'react-modal';
+import ExerciseModal from '../../components/Exercise/ExerciseModal';
 import GeneralButton from '../../components/Button/GeneralButton';
 import Start from '../../assets/svg/start.svg';
 import Stop from '../../assets/svg/stop.svg';
 import Finish from '../../assets/svg/finish.svg';
+import './Exercise.scss';
+
+Modal.setAppElement('#root');
 
 export default function Exercise() {
-  // const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedExercise, setSelectedExercise] = useState<string | string[]>('운동 선택');
   const [timer, setTimer] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
@@ -39,6 +44,13 @@ export default function Exercise() {
     return (seconds * 0.1).toFixed(2);
   };
 
+  const handleSelectExercise = (exercise: string | string[]) => {
+    if (exercise) {
+      setSelectedExercise(exercise);
+    }
+    setShowModal(false); 
+  };
+
   useEffect(() => {
     return () => {
       if (intervalId) {
@@ -48,36 +60,51 @@ export default function Exercise() {
   }, [intervalId]);
 
   return (
-    <div style={{ textAlign: 'center' }}>
-      <GeneralButton
-        buttonStyle={{ style: 'semiOutlined', size: 'mini' }}
-        onClick={() => navigate('/exercise/recommend')}>
-        운동 추천
-      </GeneralButton>
+    <div className='exerciseContainer'>
+      <div className='exerciseRecommendButton'>
+        <GeneralButton
+          buttonStyle={{ style: 'semiOutlined', size: 'mini' }}
+          onClick={() => navigate('/exercise/recommend')}
+        >
+          운동 추천
+        </GeneralButton>
+      </div>
       <div>{/* Image placeholder */}</div>
       <GeneralButton
         buttonStyle={{ style: 'primary', size: 'large' }}
-        // onClick={() => setShowModal(true)}
+        onClick={() => setShowModal(true)}
       >
-        운동 선택
+        {Array.isArray(selectedExercise) ? selectedExercise.join(', ') : selectedExercise}
       </GeneralButton>
+
+      <Modal
+        isOpen={showModal}
+        onRequestClose={() => setShowModal(false)}
+        className="modalContent"
+        overlayClassName="modalOverlay"
+      >
+        <ExerciseModal onSelectExercise={handleSelectExercise} multiple={false} />
+      </Modal>
 
       {!isFinished ? (
         <>
-          {/* {showModal && <ExerciseModal onClose={() => setShowModal(false)} />} */}
-          <div>{new Date(timer * 1000).toISOString().substr(11, 8)}</div>
-          {!isRunning ? (
-            <button onClick={startTimer}>
-              <img src={Start} alt="start" />
+          <div className='timer'>
+            {new Date(timer * 1000).toISOString().substr(11, 8)}
+          </div>
+          <div className='timerButton'>
+            {!isRunning ? (
+              <button onClick={startTimer}>
+                <img src={Start} alt="start" />
+              </button>
+            ) : (
+              <button onClick={handleStopTimer}>
+                <img src={Stop} alt="stop" />
+              </button>
+            )}
+            <button onClick={handleFinish} disabled={!isRunning && timer === 0}>
+              <img src={Finish} alt="finish" />
             </button>
-          ) : (
-            <button onClick={handleStopTimer}>
-              <img src={Stop} alt="stop" />
-            </button>
-          )}
-          <button onClick={handleFinish} disabled={!isRunning && timer === 0}>
-            <img src={Finish} alt="finish" />
-          </button>
+          </div>
         </>
       ) : (
         <div>
