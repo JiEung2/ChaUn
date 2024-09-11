dcbf22853ee0dc1acc4244b850151f5a09764806
JiEung2 <wldmd5007@naver.com>
2024-09-11 14:35:09 +0900

    feat: InfoSurveySuccessDto 추가
    - 회원 정보 설문조사 완료 dto

---
+++ project/back/health/src/main/java/com/ssafy/health/domain/account/dto/response/InfoSurveySuccessDto.java
@@ -0,0 +1,8 @@
+package com.ssafy.health.domain.account.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class InfoSurveySuccessDto {
+    private final String message = "회원 정보 설문조사를 완료했습니다.";
+}
