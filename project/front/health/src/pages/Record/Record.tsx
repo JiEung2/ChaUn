5d8a2da6ecc3f2c0e00591d2b23cd645f0751bc8
김민영 <rlaalsdud41@naver.com>
2024-09-14 21:03:50 +0900

    feat: record 페이지 컴포넌트 분리(예측 이미지&그래프, 체형예측), 인풋 컴포넌트에서 타입 지정 추가, 최솟값 추가

--- project/front/health/src/pages/Record/Record.tsx
+++ project/front/health/src/pages/Record/Record.tsx
@@ -1,24 +1,18 @@
 import './Record.scss';
 import GeneralButton from '@/components/Button/GeneralButton';
 import BodyWeightRecord from '@/components/Record/BodyWeightRecord';
-import PredictionResult from '@/components/Record/PredictionResult';
-import InputField from '@/components/Input/Input';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import ExerciseModal from '@/components/Exercise/ExerciseModal';
-import DropDown from '@/assets/svg/dropDown.svg';
-import DropUp from '@/assets/svg/dropUp.svg';
+import ExerciseInput from '@/components/Record/ExerciseInput';
 
 export default function RecordPage() {
   const navigate = useNavigate();
   const [exerciseType, setExerciseType] = useState('');
   const [duration, setDuration] = useState('');
-  const [showExerciseModal, setShowExerciseModal] = useState(false);
   const [day, setDay] = useState('');
-  const [showDayDropdown, setShowDayDropdown] = useState(false);
   const [isButtonEnabled, setIsButtonEnabled] = useState(false);
   const [exerciseDays, setExerciseDays] = useState(0);
-  const [showBodyWeightRecord, setShowBodyWeightRecord] = useState(false); // State to control visibility of BodyWeightRecord
+  const [showBodyWeightRecord, setShowBodyWeightRecord] = useState(false);
 
   const data = [
     { time: '현재', weight: 62.0 },
@@ -26,23 +20,10 @@ export default function RecordPage() {
     { time: '3달 후', weight: 58.6 },
   ];
 
-  // ExerciseModal에서 선택한 운동 종목을 처리하는 함수
-  const handleExerciseSelect = (selected: string | string[]) => {
-    const exercise = Array.isArray(selected) ? selected[0] : selected;
-    setExerciseType(exercise);
-    setShowExerciseModal(false);
-  };
-
-  // 모달을 닫는 함수
-  const handleCloseModal = () => {
-    setShowExerciseModal(false);
-  };
-
-  // 렌더링 되었을 때 운동이 며칠 진행되었는지 확인하는 함수
   useEffect(() => {
     const fetchExerciseDays = () => {
       const dummyExerciseData = {
-        exerciseDays: 2, 
+        exerciseDays: 2,
       };
       setExerciseDays(dummyExerciseData.exerciseDays);
     };
@@ -50,7 +31,6 @@ export default function RecordPage() {
     fetchExerciseDays();
   }, []);
 
-  // 모든 필드가 채워졌는지 확인하는 함수
   useEffect(() => {
     if (exerciseType && day && duration) {
       setIsButtonEnabled(true);
@@ -59,9 +39,15 @@ export default function RecordPage() {
     }
   }, [exerciseType, day, duration]);
 
-  // Handle showing BodyWeightRecord and updating the display when the button is clicked
   const handleShowBodyWeightRecord = () => {
     setShowBodyWeightRecord(true);
+  };
+
+  const handleResetInput = () => {
+    setShowBodyWeightRecord(false);
+    setExerciseType('');
+    setDuration('');
+    setDay('');
   };
 
   return (
@@ -86,79 +72,31 @@ export default function RecordPage() {
             <p className="predictionText">
               <strong>민영님</strong>의 운동 강도를 높였을 때, 체형 예측 결과를 조회해보세요
             </p>
-            <div className="exerciseInput">
-              <div className="inputBox">
-                <button
-                  className="inputField semiMedium"
-                  onClick={() => setShowExerciseModal(true)}
-                >
-                  {exerciseType || '운동 종목'}
-                </button>
-                {showExerciseModal && (
-                  <ExerciseModal
-                    onSelectExercise={handleExerciseSelect}
-                    multiple={false}
-                    onClose={handleCloseModal}
-                  />
-                )}
-
-                <button
-                  className="inputField semiSmall dayInput"
-                  onClick={() => setShowDayDropdown(!showDayDropdown)}
-                >
-                  {day || ''}
-                  <img
-                    src={showDayDropdown ? DropUp : DropDown}
-                    alt={showDayDropdown ? 'DropUp' : 'DropDown'}
-                    className="dropDown"
-                  />
-                </button>
-                {showDayDropdown && (
-                  <div className="dayDropdown">
-                    <div
-                      onClick={() => {
-                        setDay('1');
-                        setShowDayDropdown(false);
-                      }}
-                    >
-                      1
-                    </div>
-                    <div
-                      onClick={() => {
-                        setDay('2');
-                        setShowDayDropdown(false);
-                      }}
-                    >
-                      2
-                    </div>
-                  </div>
-                )}
-                일
-
-                <InputField
-                  placeholder=""
-                  value={duration}
-                  onChange={(e) => setDuration(e.target.value)}
-                  size="semiTiny"
-                />
-                분
-              </div>
-              <GeneralButton
-                buttonStyle={{ style: 'primary', size: 'medium' }}
-                disabled={!isButtonEnabled}
-                onClick={handleShowBodyWeightRecord}
-              >
-                체형 예측
-              </GeneralButton>
-            </div>
+            <ExerciseInput
+              onShowPrediction={handleShowBodyWeightRecord}
+              setExerciseType={setExerciseType}
+              setDuration={setDuration}
+              setDay={setDay}
+              isButtonEnabled={isButtonEnabled}
+              exerciseType={exerciseType}
+              duration={duration}
+              day={day}
+            />
           </div>
         ) : showBodyWeightRecord ? (
           <div className="predictionResult">
             <p className="predictionText">
-              <strong>민영님</strong>이 {exerciseType}을 하루 {duration}분, 총 {exerciseDays}일 <br />
-              추가로 진행했을 때 예측되는 체형을 알려드릴게요!
+              <strong>민영님</strong>이 <strong>{exerciseType}</strong>을 하루 <strong>{duration}분</strong>, <br />
+              <strong>총 {exerciseDays}일</strong> 추가로 진행했을 때 <br />
+              예측되는 체형을 알려드릴게요!
             </p>
-            <PredictionResult data={data} />
+            <BodyWeightRecord data={data} />
+            <GeneralButton
+              buttonStyle={{ style: 'primary', size: 'large' }}
+              onClick={handleResetInput}
+            >
+              운동 다시 입력하기
+            </GeneralButton>
           </div>
         ) : (
           <div className="exerciseAdvice">
@@ -169,12 +107,9 @@ export default function RecordPage() {
               주 2~3회 운동을 권장하고 있습니다. <br />
               다음 주도 힘내서 달려볼까요~?
             </p>
-            {/* 로띠 추가 */}
           </div>
-        )}        
+        )}
       </div>
-
-
     </div>
   );
 }
