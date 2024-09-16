57634da4ff604c86b38c7110ce28cdec4c2b1453
JiEung2 <181757@jnu.ac.kr>
2024-09-16 18:32:54 +0900

    fix: BaseEntity에 MappedSuperclass 추가

--- project/back/health/src/main/java/com/ssafy/health/common/entity/BaseEntity.java
+++ project/back/health/src/main/java/com/ssafy/health/common/entity/BaseEntity.java
@@ -2,6 +2,7 @@ package com.ssafy.health.common.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
 public class BaseEntity {
 
     @CreatedDate
