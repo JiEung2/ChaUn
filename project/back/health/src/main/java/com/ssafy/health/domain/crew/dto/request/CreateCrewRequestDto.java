1ffb3011c7bd3886cff6cccd9cf2fcf980caf356
JiEung2 <181757@jnu.ac.kr>
2024-09-17 00:53:56 +0900

    fix: CrewRegisterDto에서 CreateCrewRequestDto로 변경

--- project/back/health/src/main/java/com/ssafy/health/domain/crew/dto/request/CreateCrewRequestDto.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/crew/dto/request/CreateCrewRequestDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @Builder
-public class CrewRegisterDto {
+public class CreateCrewRequestDto {
     private String name;
     private String profileImage;
     private Float averageAge;
