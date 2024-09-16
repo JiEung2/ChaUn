1943a1b69ef5312d11dc4045153d1a98e3282b43
JiEung2 <181757@jnu.ac.kr>
2024-09-16 18:41:17 +0900

    feat: getWeightData swagger 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/body/BodyHistory/controller/BodyHistoryControllerApi.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/body/BodyHistory/controller/BodyHistoryControllerApi.java
@@ -33,4 +33,32 @@ public interface BodyHistoryControllerApi {
     })
     ApiResponse<BodySurveySuccessDto> saveBodySurvey(@RequestBody BodySurveyRequestDto bodySurveyRequestDto);
 
+    @Operation(
+            summary = "회원의 6개월 간 몸무게 조회",
+            description = "특정 회원의 6개월 간의 몸무게를 조회합니다."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "회원 몸무게 조회 완료",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(value = "{\n" +
+                                    "  \"status\": 200,\n" +
+                                    "  \"message\": \"Success\",\n" +
+                                    "  \"data\": {\n" +
+                                    "    \"weightDataList\": [\n" +
+                                    "      {\n" +
+                                    "        \"date\": \"2024-09-16T15:00:00\",\n" +
+                                    "        \"weight\": 75.5\n" +
+                                    "      },\n" +
+                                    "      {\n" +
+                                    "        \"date\": \"2024-08-16T15:00:00\",\n" +
+                                    "        \"weight\": 74.0\n" +
+                                    "      }\n" +
+                                    "    ]\n" +
+                                    "  }\n" +
+                                    "}"
+                            ))
+            )
+    })
+    ApiResponse<WeightHistoryResponseDto> getWeightData(@PathVariable("user_id") Long userId);
+
 }
