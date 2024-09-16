03a0cec978987a5ad7116ee74027fc8c4c8e5542
JiEung2 <181757@jnu.ac.kr>
2024-09-16 20:38:38 +0900

    feat: getBodyHistory 기능 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/body/BodyHistory/service/BodyHistoryReadService.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/body/BodyHistory/service/BodyHistoryReadService.java
@@ -2,9 +2,13 @@ package com.ssafy.health.domain.body.BodyHistory.service;
 
 import static java.time.temporal.TemporalAdjusters.*;
 
+import com.ssafy.health.common.security.SecurityUtil;
+import com.ssafy.health.domain.body.BodyHistory.dto.request.BodyHistoryRequestDto;
+import com.ssafy.health.domain.body.BodyHistory.dto.response.BodyHistoryResponseDto;
 import com.ssafy.health.domain.body.BodyHistory.dto.response.WeightHistoryResponseDto;
 import com.ssafy.health.domain.body.BodyHistory.entity.BodyHistory;
 import com.ssafy.health.domain.body.BodyHistory.repository.BodyHistoryRepository;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
@@ -38,4 +42,30 @@ public class BodyHistoryReadService {
                 .weightDataList(weightDataList)
                 .build();
     }
+
+    public BodyHistoryResponseDto getBodyHistory(BodyHistoryRequestDto requestDto) {
+
+        LocalDate startDate = LocalDate.of(requestDto.getYear(), requestDto.getMonth(), 1);
+        LocalDate endDate = startDate.withDayOfMonth(startDate.lengthOfMonth());
+
+        LocalDateTime startDateTime = startDate.atStartOfDay();
+        LocalDateTime endDateTime = endDate.atTime(LocalTime.MAX);
+
+        List<BodyHistory> bodyHistoryList = bodyHistoryRepository.findByUserIdAndCreatedAtBetween(
+                SecurityUtil.getCurrentUserId(), startDateTime, endDateTime);
+
+        List<BodyHistoryResponseDto.BodyHistoryData> bodyHistoryDataList = bodyHistoryList.stream()
+                .map(bodyHistory -> BodyHistoryResponseDto.BodyHistoryData.builder()
+                        .bodyFatRatio(bodyHistory.getBodyFatRatio())
+                        .skeletalMuscleMass(bodyHistory.getSkeletalMuscleMass())
+                        .weight(bodyHistory.getWeight())
+                        .date(bodyHistory.getCreatedAt())
+                        .build())
+                .collect(Collectors.toList());
+
+        return BodyHistoryResponseDto.builder()
+                .bodyHistoryDataList(bodyHistoryDataList)
+                .build();
+    }
+
 }
