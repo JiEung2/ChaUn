5b5546e3b8a0bfd03f38f86545b02c769ddfbb11
JiEung2 <181757@jnu.ac.kr>
2024-09-18 00:02:27 +0900

    feat: getJoinedCrewList Swagger 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/crew/controller/CrewControllerApi.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/crew/controller/CrewControllerApi.java
@@ -3,11 +3,13 @@ package com.ssafy.health.domain.crew.controller;
 import com.ssafy.health.common.ApiResponse;
 import com.ssafy.health.domain.crew.dto.request.CreateCrewRequestDto;
 import com.ssafy.health.domain.crew.dto.response.CreateCrewSuccessDto;
+import com.ssafy.health.domain.crew.dto.response.CrewListResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "크루 컨트롤러", description = "크루 생성, 조회, 삭제 등 크루를 관리하는 클래스")
@@ -30,5 +32,39 @@ public interface CrewControllerApi {
     })
     ApiResponse<CreateCrewSuccessDto> createCrew(@RequestBody CreateCrewRequestDto createCrewRequestDto);
 
+    @Operation(
+            summary = "가입된 크루 조회",
+            description = "회원의 가입된 크루 조회"
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200",
+                    description = "가입된 크루 조회 성공",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(value = "{\n" +
+                                    "  \"status\": 200,\n" +
+                                    "  \"message\": \"\",\n" +
+                                    "  \"data\": {\n" +
+                                    "    \"crewList\": [\n" +
+                                    "      {\n" +
+                                    "        \"crewName\": \"달리자\",\n" +
+                                    "        \"exerciseName\": \"러닝\",\n" +
+                                    "        \"crewProfileImage\": \"crew-profile-image.jpg\"\n" +
+                                    "      },\n" +
+                                    "      {\n" +
+                                    "        \"crewName\": \"달리자\",\n" +
+                                    "        \"exerciseName\": \"러닝\",\n" +
+                                    "        \"crewProfileImage\": \"crew-profile-image.jpg\"\n" +
+                                    "      }\n" +
+                                    "    ]\n" +
+                                    "  }\n" +
+                                    "}"
+                            )
+                    )
+            )
+    })
+
+    ApiResponse<CrewListResponseDto> getJoinedCrewList(@PathVariable("user_id") Long userId);
 
 }
