4797258e196b6d9697981f3f47910c54b8337ff4
JiEung2 <181757@jnu.ac.kr>
2024-09-18 00:02:18 +0900

    feat: getJoinedCrewList 컨트롤러 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/crew/controller/CrewController.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/crew/controller/CrewController.java
@@ -5,8 +5,12 @@ import com.ssafy.health.domain.account.dto.request.InfoSurveyRequestDto;
 import com.ssafy.health.domain.account.dto.response.InfoSurveySuccessDto;
 import com.ssafy.health.domain.crew.dto.request.CreateCrewRequestDto;
 import com.ssafy.health.domain.crew.dto.response.CreateCrewSuccessDto;
+import com.ssafy.health.domain.crew.dto.response.CrewListResponseDto;
+import com.ssafy.health.domain.crew.service.CrewReadService;
 import com.ssafy.health.domain.crew.service.CrewWriteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
-public class CrewController implements CrewControllerApi{
+public class CrewController implements CrewControllerApi {
 
     private final CrewWriteService crewWriteService;
+    private final CrewReadService crewReadService;
 
     @PostMapping("/crew")
     public ApiResponse<CreateCrewSuccessDto> createCrew(@RequestBody CreateCrewRequestDto createCrewRequestDto) {
         return ApiResponse.success(crewWriteService.createCrew(createCrewRequestDto));
+    }
+
+    @GetMapping("/users/{user_id}/crew-list")
+    public ApiResponse<CrewListResponseDto> getJoinedCrewList(@PathVariable("user_id") Long userId) {
+        return ApiResponse.success(crewReadService.getJoinedCrewList(userId));
     }
 }
