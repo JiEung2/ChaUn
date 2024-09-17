e50a0f0970c202f6a2055c071a638bbbf0b84534
JiEung2 <181757@jnu.ac.kr>
2024-09-18 00:01:31 +0900

    feat: UserCrewRepository에 findByUserId 쿼리 메서드 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/account/repository/UserCrewRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/account/repository/UserCrewRepository.java
@@ -1,7 +1,9 @@
 package com.ssafy.health.domain.account.repository;
 
 import com.ssafy.health.domain.account.entity.UserCrew;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserCrewRepository extends JpaRepository<UserCrew, Long> {
+    List<UserCrew> findByUserId(Long userId);
 }
