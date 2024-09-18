a2358cbb4aae8f46f9495843e2c664a66d4923eb
JiEung2 <181757@jnu.ac.kr>
2024-09-18 22:12:00 +0900

    feat: UserCrewRepository에 findByCrewId 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/account/repository/UserCrewRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/account/repository/UserCrewRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserCrewRepository extends JpaRepository<UserCrew, Long> {
     List<UserCrew> findByUserId(Long userId);
+
+    List<UserCrew> findByCrewId(Long crewId);
 }
