6ba5b0e2ed8a1e0c79067974017997b7b1589ecc
MINJOO-KIM <gkminju2000@gmail.com>
2024-09-19 09:55:46 +0900

    feat: 버튼 추가

--- project/front/health/src/pages/Crew/Crew.tsx
+++ project/front/health/src/pages/Crew/Crew.tsx
@@ -1,5 +1,10 @@
 import BattleBoard from './components/BattleBoard';
 // import CrewList from './components/CrewList';
+import StyledButton from '../../components/Button/StyledButton';
+import { useNavigate } from 'react-router-dom';
+import createIcon from '../../assets/svg/crewCreate.svg';
+import recommendIcon from '../../assets/svg/crewRecommend.svg';
+import rankingIcon from '../../assets/svg/crewRanking.svg';
 
 export default function CrewPage() {
   const crews = [
@@ -8,6 +13,7 @@ export default function CrewPage() {
     { id: 3, name: '달리는 번개', tag: '#러닝' },
     { id: 4, name: '달리는 번개', tag: '#러닝' },
   ];
+  const navigate = useNavigate();
   return (
     <>
       <div className="my-crew">
@@ -24,6 +30,30 @@ export default function CrewPage() {
         opponentTeamScore={1200}
         dDay="3"
       />
+      <div className="buttonSection">
+        <div className="crewButtonSection">
+          <StyledButton
+            title="크루 생성"
+            icon={createIcon}
+            onClick={() => navigate('/crew/create')}
+            backgroundColor="styledButton1"
+          />
+          <StyledButton
+            title="크루 추천"
+            icon={recommendIcon}
+            onClick={() => navigate('/crew/recommend')}
+            backgroundColor="styledButton2"
+          />
+        </div>
+        <div className="rankingButtonSection">
+          <StyledButton
+            title="실시간 크루랭킹"
+            icon={rankingIcon}
+            onClick={() => navigate('/crew/ranking')}
+            backgroundColor="styledButton3"
+          />
+        </div>
+      </div>
     </>
   );
 }
