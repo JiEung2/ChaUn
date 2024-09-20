fc27caede5bd7755b417392749ab9513a4d7d15a
JiEung2 <wldmd5007@naver.com>
2024-09-20 13:35:13 +0900

    feat: 크루 코인을 사용하는 메서드 구현
    - 이 과정에서 spendCoins라고 되어있던 메서드명을 구분을 하기 위해 spendUserCoins라고 변경

--- project/back/health/src/main/java/com/ssafy/health/domain/coin/service/CoinService.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/coin/service/CoinService.java
@@ -3,6 +3,9 @@ package com.ssafy.health.domain.coin.service;
 import com.ssafy.health.domain.account.entity.User;
 import com.ssafy.health.domain.account.exception.UserNotFoundException;
 import com.ssafy.health.domain.account.repository.UserRepository;
+import com.ssafy.health.domain.crew.entity.Crew;
+import com.ssafy.health.domain.crew.exception.CrewNotFoundException;
+import com.ssafy.health.domain.crew.repository.CrewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,16 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CoinService {
 
-    private final UserRepository userRepository;
     private final CoinValidator coinValidator;
 
-    public void spendCoins(Long userId, Integer coins) {
-        User user = findUserById(userId);
+    public void spendUserCoins(final User user, final Integer coins) {
         coinValidator.validateSufficientCoins(user.getCoin(), coins);
         user.decreaseCoin(coins);
     }
 
-    private User findUserById(Long userId) {
-        return userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+    public void spendCrewCoins(final Crew crew, final Integer coins) {;
+        crew.decreaseCoin(coins);
     }
 }
