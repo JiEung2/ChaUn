40eee8f3668abe658f2ed003f8cf1dd4dd482aba
인호현 <inhohyun36@gmail.com>
2024-09-22 16:40:53 +0900

    design: 설문조사 4번 레이아웃 조정

--- project/front/health/src/components/Survey/4.tsx
+++ project/front/health/src/components/Survey/4.tsx
@@ -1,6 +1,7 @@
 import GeneralButton from '../Button/GeneralButton';
 import ExerciseCategories from '../Exercise/ExerciseCategories';
 import { useState } from 'react';
+import styles from './4.module.scss';
 interface FourProps {
   //TODO - 성공 화면으로 이동
   finishServey: () => void;
@@ -16,16 +17,18 @@ export default function Four({ finishServey, handlePrev }: FourProps) {
     }
   };
   return (
-    <div>
+    <div className={styles.container}>
       <h1>선호하는 운동</h1>
       <h3>최대 5개까지 선택 가능합니다.</h3>
       <ExerciseCategories onSelect={handleSelectExercises} multiple={true} />
-      <GeneralButton buttonStyle={{ style: 'semiOutlined', size: 'tiny' }} onClick={handlePrev}>
-        이전
-      </GeneralButton>
-      <GeneralButton buttonStyle={{ style: 'semiPrimary', size: 'tiny' }} onClick={finishServey}>
-        완료
-      </GeneralButton>
+      <div className={styles.buttonGroup}>
+        <GeneralButton buttonStyle={{ style: 'semiOutlined', size: 'tiny' }} onClick={handlePrev}>
+          이전
+        </GeneralButton>
+        <GeneralButton buttonStyle={{ style: 'semiPrimary', size: 'tiny' }} onClick={finishServey}>
+          완료
+        </GeneralButton>
+      </div>
     </div>
   );
 }
