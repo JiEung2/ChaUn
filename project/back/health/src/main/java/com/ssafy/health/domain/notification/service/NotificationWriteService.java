d4ca9a9033037e8927fd5ce40a3fa6f39a2f43ce
송준혁 <fprhqkrtk303@naver.com>
2024-09-23 11:20:17 +0900

    feat: 사용자에게 등록된 기기가 있을 경우에만 FCM 전송
    S11P21C106-570, S11P21C106-573, S11P21C106-574

--- project/back/health/src/main/java/com/ssafy/health/domain/notification/service/NotificationWriteService.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/notification/service/NotificationWriteService.java
@@ -1,5 +1,10 @@
 package com.ssafy.health.domain.notification.service;
 
+import com.ssafy.health.common.fcm.dto.request.FcmRequestDto;
+import com.ssafy.health.common.fcm.service.FcmService;
+import com.ssafy.health.domain.account.entity.User;
+import com.ssafy.health.domain.account.exception.UserNotFoundException;
+import com.ssafy.health.domain.account.repository.UserRepository;
 import com.ssafy.health.domain.battle.entity.BattleStatus;
 import com.ssafy.health.domain.battle.repository.BattleRepository;
 import com.ssafy.health.domain.body.BodyHistory.repository.BodyHistoryRepository;
@@ -10,11 +15,13 @@ import com.ssafy.health.domain.notification.entity.NotificationStatus;
 import com.ssafy.health.domain.notification.entity.NotificationType;
 import com.ssafy.health.domain.notification.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 import static com.ssafy.health.domain.notification.entity.NotificationMessage.*;
 
@@ -25,8 +32,12 @@ public class NotificationWriteService {
     private final BattleRepository battleRepository;
     private final BodyHistoryRepository bodyHistoryRepository;
     private final NotificationRepository notificationRepository;
+    private final UserRepository userRepository;
+    private final FcmService fcmService;
 
-    public void createBodySurveyNotification(NotificationRequestDto dto) {
+    public void createBodySurveyNotification(NotificationRequestDto dto)
+            throws ExecutionException, InterruptedException {
+
         Map<String, Object> additionalData = new HashMap<>();
         LocalDateTime lastSurveyedDate = null;
 
@@ -41,9 +52,14 @@ public class NotificationWriteService {
         Notification notification = notificationBuilder(
                 dto.getNotificationType(), dto.getUserId(), SURVEY.getMessage(), additionalData);
         notificationRepository.save(notification);
+
+        // 사용자에 등록된 기기가 있을 경우에만 FCM 푸시
+        sendFcmMessage(dto.getUserId(), "체형 입력 알림", SURVEY.getMessage());
     }
 
-    public void createBattleNotification(NotificationRequestDto dto, Long battleId) {
+    public void createBattleNotification(NotificationRequestDto dto, Long battleId)
+            throws ExecutionException, InterruptedException {
+
         Map<String, Object> additionalData = new HashMap<>();
         additionalData.put("battleId", battleId);
 
@@ -53,6 +69,9 @@ public class NotificationWriteService {
         Notification battleNotification = notificationBuilder(
                 dto.getNotificationType(), dto.getUserId(), message, additionalData);
         notificationRepository.save(battleNotification);
+
+        // 사용자에 등록된 기기가 있을 경우에만 FCM 푸시
+        sendFcmMessage(dto.getUserId(), "배틀 알림", message);
     }
 
     public StatusUpdateResponseDto updateNotificationStatus(NotificationStatus status, Long notificationId) {
@@ -65,6 +84,20 @@ public class NotificationWriteService {
                 .previousStatus(previousStatus)
                 .currentStatus(status)
                 .build();
+    }
+
+    public void sendFcmMessage(Long userId, String title, String body) throws ExecutionException, InterruptedException {
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+        String token = user.getDeviceToken();
+
+        if (token != null) {
+            FcmRequestDto fcmRequestDto = FcmRequestDto.builder()
+                    .token(token)
+                    .title(title)
+                    .body(body)
+                    .build();
+            fcmService.sendMessageToDevice(fcmRequestDto);
+        }
     }
 
     public Notification notificationBuilder(NotificationType notificationType, Long userId, String message) {
