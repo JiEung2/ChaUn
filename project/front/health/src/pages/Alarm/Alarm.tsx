5c1a91dfd2a2907b30a6e4459c08901a0a694fd5
minyeong981 <rlaalsdud41@naver.com>
2024-09-23 14:15:44 +0900

    style: homePage 세미콜론 추가

--- project/front/health/src/pages/Alarm/Alarm.tsx
+++ project/front/health/src/pages/Alarm/Alarm.tsx
@@ -1,18 +1,38 @@
-import AlarmItem from '@/components/Alarm/AlarmItem';
+// import AlarmItem from '@/components/Alarm/AlarmItem';
 
-const alarmData = [
-  { type: 'prediction', date: '24.07.01', time: '08:02' },
-  { type: 'prediction', date: '24.07.01', time: '08:02' },
-  { type: 'battleEnded', date: '24.07.01', time: '08:02' },
-  { type: 'battleStarted', date: '24.07.01', time: '08:02' },
-];
+// type AlarmData = {
+//   notificationId: number,
+//   notificationType: string,
+//   additionalData: string,
+//   content: string,
+//   createdAt: Date,
+
+// }
+// const alarmData = [
+//     {
+//       "notificationId": 1,
+//       "notificationType": "BODY_SURVEY",
+//       "additionalData": null,
+//       "content": "테스트 알림 1",
+//       "createdAt": "2024-09-20T00:00:00"
+//     },
+//     {
+//       "notificationId": 2,
+//       "notificationType": "BATTLE",
+//       "additionalData": {
+//           "battleId": 1
+//       },
+//       "content": "테스트 알림 2",
+//       "createdAt": "2024-09-20T00:00:00"
+//     },
+// ];
 
 export default function AlarmPage() {
   return (
     <div className="alarmContainer">
-      {alarmData.map((alarm, index) => (
+      {/* {alarmData.map((alarm, index) => (
         <AlarmItem key={index} type={alarm.type} date={alarm.date} time={alarm.time} />
-      ))}
+      ))} */}
     </div>
   );
 }
--- project/front/health/src/pages/Home/Home.tsx
+++ project/front/health/src/pages/Home/Home.tsx
@@ -204,4 +204,4 @@ export default function HomePage() {
       </div>
     </div>
   );
-}
+};
