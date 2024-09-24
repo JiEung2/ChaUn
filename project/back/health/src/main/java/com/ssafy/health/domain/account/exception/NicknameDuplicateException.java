537892382461956fe39f887f87e0ce9fd00897c3
JiEung2 <wldmd5007@naver.com>
2024-09-24 15:40:30 +0900

    fix: USER_NOT_FOUND에서 USER_NICKNAME_DUPLICATED로 수정

--- project/back/health/src/main/java/com/ssafy/health/domain/account/exception/NicknameDuplicateException.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/account/exception/NicknameDuplicateException.java
@@ -3,9 +3,9 @@ package com.ssafy.health.domain.account.exception;
 public class NicknameDuplicateException extends RuntimeException {
 
     public String getMessage() {
-        return UserExceptionMessage.USER_NOT_FOUND.getMessage();
+        return UserExceptionMessage.USER_NICKNAME_DUPLICATED.getMessage();
     }
     public int getStatus() {
-        return UserExceptionMessage.USER_NOT_FOUND.getStatus();
+        return UserExceptionMessage.USER_NICKNAME_DUPLICATED.getStatus();
     }
 }
