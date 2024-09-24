8ee50fe943c4290036b207b2367b33dd9d88d6d3
송준혁 <fprhqkrtk303@naver.com>
2024-09-24 21:46:36 +0900

    feat: 크루 추천을 위한 레포지토리 생성
    크루 추천 레포지토리는 MongoDB JPA가 관리

---
+++ project/back/health/src/main/java/com/ssafy/health/domain/account/repository/RecommendedCrewRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.health.domain.account.repository;
+
+import com.ssafy.health.domain.account.entity.RecommendedCrew;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RecommendedCrewRepository extends MongoRepository<RecommendedCrew, Long> {
+}
