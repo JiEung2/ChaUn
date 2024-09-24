3e306096bfd0187b86253a67086f581000e2fafc
JiEung2 <wldmd5007@naver.com>
2024-09-24 13:31:15 +0900

    feat: Crew에 exercise 인덱스 추가
    - Crew의 조회 효율을 높이기 위함

--- project/back/health/src/main/java/com/ssafy/health/domain/crew/entity/Crew.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/crew/entity/Crew.java
@@ -3,19 +3,16 @@ package com.ssafy.health.domain.crew.entity;
 import com.ssafy.health.common.entity.BaseEntity;
 import com.ssafy.health.domain.crew.dto.request.CreateCrewRequestDto;
 import com.ssafy.health.domain.exercise.entity.Exercise;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Version;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = {
+        @Index(name = "idx_exercise_id", columnList = "exercise_id"),
+})
 public class Crew extends BaseEntity {
 
     @Id
