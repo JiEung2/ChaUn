a5e461f653a1c94e4723df3654545f43cfe4ddd7
송준혁 <fprhqkrtk303@naver.com>
2024-09-24 21:47:04 +0900

    feat: 유저 ID를 사용한 추천 크루 검색 메서드 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/account/repository/RecommendedCrewRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/account/repository/RecommendedCrewRepository.java
@@ -4,4 +4,6 @@ import com.ssafy.health.domain.account.entity.RecommendedCrew;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface RecommendedCrewRepository extends MongoRepository<RecommendedCrew, Long> {
+
+    RecommendedCrew findByUserId(Long userId);
 }
