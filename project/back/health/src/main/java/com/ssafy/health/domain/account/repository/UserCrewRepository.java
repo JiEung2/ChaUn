a4865a92f65680385eb640b8a88fe7c24d8a31c6
JiEung2 <wldmd5007@naver.com>
2024-09-24 13:29:26 +0900

    feat: UserCrew의 basicScore를 업데이트 하는 쿼리 추가
    - updateBasicScoreByUserAndExercise

--- project/back/health/src/main/java/com/ssafy/health/domain/account/repository/UserCrewRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/account/repository/UserCrewRepository.java
@@ -1,11 +1,25 @@
 package com.ssafy.health.domain.account.repository;
 
+import com.ssafy.health.domain.account.entity.User;
 import com.ssafy.health.domain.account.entity.UserCrew;
 import java.util.List;
+
+import com.ssafy.health.domain.exercise.entity.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserCrewRepository extends JpaRepository<UserCrew, Long> {
     List<UserCrew> findByUserId(Long userId);
-
     List<UserCrew> findByCrewId(Long crewId);
+
+    @Modifying
+    @Query("UPDATE UserCrew uc " +
+            "SET uc.basicScore = uc.basicScore + :basicScore " +
+            "WHERE uc.user = :user " +
+            "AND uc.crew IN (SELECT c FROM Crew c WHERE c.exercise = :exercise)")
+    void updateBasicScoreByUserAndExercise(@Param("user") User user,
+                                           @Param("exercise") Exercise exercise,
+                                           @Param("basicScore") Float basicScore);
 }
