c75740cfce45b82827ee79dad1affb9317f6e59f
송준혁 <fprhqkrtk303@naver.com>
2024-09-24 21:45:34 +0900

    feat: MongoDB 관리 어노테이션 추가

--- project/back/health/src/main/java/com/ssafy/health/HealthApplication.java
+++ project/back/health/src/main/java/com/ssafy/health/HealthApplication.java
@@ -3,9 +3,11 @@ package com.ssafy.health;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableMongoAuditing
 public class HealthApplication {
 
 	public static void main(String[] args) {
