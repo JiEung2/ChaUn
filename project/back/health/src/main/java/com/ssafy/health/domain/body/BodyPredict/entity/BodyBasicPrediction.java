43e8b3e8b3337ab76ad323d51d22493d92d0ff73
송준혁 <fprhqkrtk303@naver.com>
2024-09-25 22:19:17 +0900

    feat: 기본 체형 예측 정보 엔티티

---
+++ project/back/health/src/main/java/com/ssafy/health/domain/body/BodyPredict/entity/BodyBasicPrediction.java
@@ -0,0 +1,21 @@
+package com.ssafy.health.domain.body.BodyPredict.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@Document(collection = "predict_basic")
+public class BodyBasicPrediction {
+
+    @Field("user_id")
+    private Long userId;
+    private Float p30;
+    private Float p90;
+    @Field("create_at")
+    private LocalDateTime createdAt;
+}
