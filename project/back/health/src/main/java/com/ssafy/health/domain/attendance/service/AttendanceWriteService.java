e09ae8e3b41c3af7028f9ec3430598e84b0ae33a
JiEung2 <181757@jnu.ac.kr>
2024-09-25 21:08:06 +0900

    feat: AttendanceWriteService 추가 및 markAttendance 구현
    - 출석 체크를 하는 기능 구현

---
+++ project/back/health/src/main/java/com/ssafy/health/domain/attendance/service/AttendanceWriteService.java
@@ -0,0 +1,35 @@
+package com.ssafy.health.domain.attendance.service;
+
+import com.ssafy.health.common.security.SecurityUtil;
+import com.ssafy.health.domain.account.entity.User;
+import com.ssafy.health.domain.account.exception.UserNotFoundException;
+import com.ssafy.health.domain.account.repository.UserRepository;
+import com.ssafy.health.domain.attendance.dto.response.AttendanceSuccessDto;
+import com.ssafy.health.domain.attendance.entity.Attendance;
+import com.ssafy.health.domain.attendance.repository.AttendanceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AttendanceWriteService {
+
+    private final UserRepository userRepository;
+    private final AttendanceValidator attendanceValidator;
+    private final AttendanceRepository attendanceRepository;
+
+    public AttendanceSuccessDto markAttendance() {
+        attendanceValidator.validateTodayAttendance();
+
+        User user = findUserById(SecurityUtil.getCurrentUserId());
+        attendanceRepository.save(Attendance.builder().user(user).build());
+
+        return new AttendanceSuccessDto();
+    }
+
+    private User findUserById(Long userId) {
+        return userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+    }
+}
