3d375d9b918b50a16f01190df35075b95d3fdcdd
JiEung2 <181757@jnu.ac.kr>
2024-09-25 21:43:00 +0900

    fix: validateTodayAttendance 로직 수정
    -findByUserIdAndCreatedAtBetween 의 재활용을 위해 타입을 List로 변경으로 인해 optional의 예외처리로 이루어지던 부분이 List의 예외처리로 변경

--- project/back/health/src/main/java/com/ssafy/health/domain/attendance/service/AttendanceValidator.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/attendance/service/AttendanceValidator.java
@@ -1,11 +1,13 @@
 package com.ssafy.health.domain.attendance.service;
 
 import com.ssafy.health.common.security.SecurityUtil;
+import com.ssafy.health.domain.attendance.entity.Attendance;
 import com.ssafy.health.domain.attendance.exception.AttendanceAlreadyExistsException;
 import com.ssafy.health.domain.attendance.repository.AttendanceRepository;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,9 +21,11 @@ public class AttendanceValidator {
         LocalDateTime startTime = LocalDate.now().atStartOfDay();
         LocalDateTime endTime = LocalDate.now().atTime(LocalTime.MAX);
 
-        attendanceRepository.findByUserIdAndCreatedAtBetween(
-                SecurityUtil.getCurrentUserId(), startTime, endTime).ifPresentOrElse(attendance -> {
-                    throw new AttendanceAlreadyExistsException();
-                    }, () -> {});
+        List<Attendance> attendances = attendanceRepository.findByUserIdAndCreatedAtBetween(
+                SecurityUtil.getCurrentUserId(), startTime, endTime);
+
+        if (!attendances.isEmpty()) {
+            throw new AttendanceAlreadyExistsException();
+        }
     }
 }
