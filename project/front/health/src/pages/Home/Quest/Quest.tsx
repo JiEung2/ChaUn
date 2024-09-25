00ea684e757a9373f4990b3a8ae828edde5683b6
김민주 <kminju2000@naver.com>
2024-09-25 10:25:07 +0900

    design: 퀘스트 font-size 충돌 수정

--- project/front/health/src/pages/Home/Quest/Quest.tsx
+++ project/front/health/src/pages/Home/Quest/Quest.tsx
@@ -1,35 +1,30 @@
 import QuestItem from '../../../components/Home/Quest/QuestItem';
 import './Quest.scss';
 
-export default function QuestPage () {
+export default function QuestPage() {
   const todayQuests = [
     { title: '매일 몸무게 입력하기', completed: true },
     { title: '하루 한 번 운동하기', completed: true },
     { title: '크루원 1회 찌르기', completed: false },
   ];
 
-  const monthlyQuests = [
-    { title: '2주 출석 도장 찍기', completed: false },
-  ];
+  const monthlyQuests = [{ title: '2주 출석 도장 찍기', completed: false }];
 
   return (
     <div className="questList">
       <div className="todayQuest">
-        <p>오늘의 퀘스트</p>        
+        <p className="questH3">오늘의 퀘스트</p>
         {todayQuests.map((quest, index) => (
           <QuestItem key={index} title={quest.title} completed={quest.completed} />
-        ))}        
+        ))}
       </div>
-
 
       <div className="monthlyQuest">
-        <p>월간 퀘스트</p>
+        <p className="questH3">월간 퀘스트</p>
         {monthlyQuests.map((quest, index) => (
           <QuestItem key={index} title={quest.title} completed={quest.completed} />
-        ))}        
+        ))}
       </div>
-
     </div>
   );
-};
-
+}
