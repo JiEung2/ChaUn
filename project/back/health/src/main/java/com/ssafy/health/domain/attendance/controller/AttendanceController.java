6a249515db64b20807e20603d695c120da25ea87
JiEung2 <181757@jnu.ac.kr>
2024-09-25 21:45:29 +0900

    feat: getMonthlyAttendance 컨트롤러 구현

--- project/back/health/src/main/java/com/ssafy/health/domain/attendance/controller/AttendanceController.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/attendance/controller/AttendanceController.java
@@ -1,9 +1,13 @@
 package com.ssafy.health.domain.attendance.controller;
 
 import com.ssafy.health.common.ApiResponse;
+import com.ssafy.health.domain.attendance.dto.response.AttendanceListDto;
 import com.ssafy.health.domain.attendance.dto.response.AttendanceSuccessDto;
+import com.ssafy.health.domain.attendance.service.AttendanceReadService;
 import com.ssafy.health.domain.attendance.service.AttendanceWriteService;
+import com.ssafy.health.domain.exercise.dto.request.MonthlyExerciseHistoryRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AttendanceController {
 
     private final AttendanceWriteService attendanceWriteService;
+    private final AttendanceReadService attendanceReadService;
 
     @PostMapping("/attendance")
     public ApiResponse<AttendanceSuccessDto> markAttendance() {
         return ApiResponse.success(attendanceWriteService.markAttendance());
+    }
+
+    @GetMapping("/attendance")
+    public ApiResponse<AttendanceListDto> getMonthlyAttendance(MonthlyExerciseHistoryRequestDto requestDto){
+        return ApiResponse.success(attendanceReadService.getMonthlyAttendance(requestDto));
     }
 }
