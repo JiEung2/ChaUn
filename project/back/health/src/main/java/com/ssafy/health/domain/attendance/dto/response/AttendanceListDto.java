aa117115b3951d40b6c544cc5f96555470e5870c
JiEung2 <181757@jnu.ac.kr>
2024-09-25 23:52:04 +0900

    feat: LocalDateTime json format 설정 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/attendance/dto/response/AttendanceListDto.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/attendance/dto/response/AttendanceListDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.health.domain.attendance.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
@@ -8,5 +9,6 @@ import lombok.Getter;
 @Getter
 @Builder
 public class AttendanceListDto {
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     List<LocalDateTime> attendanceList;
 }
