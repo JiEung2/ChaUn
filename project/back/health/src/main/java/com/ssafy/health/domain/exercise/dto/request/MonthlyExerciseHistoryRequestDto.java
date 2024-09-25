97631c8932b80f70ad206c63034cfc79e5cfaa1f
JiEung2 <181757@jnu.ac.kr>
2024-09-25 22:12:02 +0900

    fix: 생성자 추가 및 컨트롤러 변경
    - request body로 되어있는 실수 수정

--- project/back/health/src/main/java/com/ssafy/health/domain/exercise/dto/request/MonthlyExerciseHistoryRequestDto.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/exercise/dto/request/MonthlyExerciseHistoryRequestDto.java
@@ -1,8 +1,10 @@
 package com.ssafy.health.domain.exercise.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class MonthlyExerciseHistoryRequestDto {
     private int year;
     private int month;
