cdf034dc592de94f0e92fb1aef0655c03f5269c5
JiEung2 <181757@jnu.ac.kr>
2024-09-25 22:13:10 +0900

    feat: getMonthlyAttendance swagger 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/attendance/controller/AttendanceControllerApi.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/attendance/controller/AttendanceControllerApi.java
@@ -1,7 +1,9 @@
 package com.ssafy.health.domain.attendance.controller;
 
 import com.ssafy.health.common.ApiResponse;
+import com.ssafy.health.domain.attendance.dto.response.AttendanceListDto;
 import com.ssafy.health.domain.attendance.dto.response.AttendanceSuccessDto;
+import com.ssafy.health.domain.exercise.dto.request.MonthlyExerciseHistoryRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
@@ -39,5 +41,35 @@ public interface AttendanceControllerApi {
             ),
     })
     ApiResponse<AttendanceSuccessDto> markAttendance();
+
+
+    @Operation(
+            summary = "자신의 특정 월의 출석 기록 조회",
+            description = "자신의 특정 월의 출석 기록을 조회합니다."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "출석 기록 조회 완료",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(value = """
+                                {
+                                  "status": 200,
+                                  "message": "",
+                                  "data": {
+                                    "attendanceList": [
+                                        {
+                                          "2024-09-16T15:00:00"
+                                        },
+                                        {
+                                          "2024-09-17T10:00:00"
+                                        }
+                                    ]
+                                  }
+                                }
+                                """
+                            )
+                    )
+            ),
+    })
+    ApiResponse<AttendanceListDto> getMonthlyAttendance(MonthlyExerciseHistoryRequestDto requestDto);
 
 }
