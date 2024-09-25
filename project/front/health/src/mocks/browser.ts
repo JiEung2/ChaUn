c3c549d31a1108d3f6698a02f4f72d1408d14706
인호현 <inhohyun36@gmail.com>
2024-09-25 15:06:29 +0900

    fix: msw worker 에러 해결

--- project/front/health/src/mocks/browser.ts
+++ project/front/health/src/mocks/browser.ts
@@ -3,7 +3,7 @@ import { setupWorker } from 'msw/browser';
 import { surveyHandlers } from './surveyHandlers';
 import { recordHandlers } from './recordHandlers';
 // import { crewHandlers } from './crewHandlers';
-export const recordWorker = setupWorker(...recordHandlers);
+export const worker = setupWorker(...recordHandlers, ...surveyHandlers);
 // 서비스 워커 설정
-export const surveyWorker = setupWorker(...surveyHandlers);
+// export const surveyWorker = setupWorker(...surveyHandlers);s
 // export const crewWorker = setupWorker(...crewHandlers);
--- project/front/health/src/mocks/recordHandlers.ts
+++ project/front/health/src/mocks/recordHandlers.ts
@@ -12,17 +12,18 @@ const bodyHistoryDataList = [
 ];
 export const recordHandlers = [
   // 월 당 나의 체형 조회
+
   http.get(`${baseUrl}/users/body`, ({ request }) => {
     const url = new URL(request.url);
-    const year = url.searchParams.get('year');
-    const month = url.searchParams.get('month');
-    console.log(year, month);
+    try {
+      const year = url.searchParams.get('year');
+      const month = url.searchParams.get('month');
+      console.log('msw에서 받은 year, month', year, month);
 
-    return HttpResponse.json(
-      // { data: bodyHistoryDataList },
-      {
-        status: 200,
-      }
-    );
+      return HttpResponse.json({ bodyHistoryDataList }, { status: 200 });
+    } catch (error) {
+      console.error('msw handler에서 에러 발생', error);
+      return HttpResponse.error();
+    }
   }),
 ];
