e6facd60f9e02adb6214d3906408c8283d138e73
JiEung2 <wldmd5007@naver.com>
2024-09-25 13:58:34 +0900

    fix: Score 타입 변경
    - score가 Float인 관계로 변경
    - fromEntity 삭제
    - crew점수가 crew 객체가 가지고 있는 것이 아닌 따로 계산해야되기 때문에

--- project/back/health/src/main/java/com/ssafy/health/domain/crew/dto/response/CrewScoreResponseDto.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/crew/dto/response/CrewScoreResponseDto.java
@@ -1,20 +1,11 @@
 package com.ssafy.health.domain.crew.dto.response;
 
-
-import com.ssafy.health.domain.crew.entity.Crew;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class CrewScoreResponseDto {
-    private Integer basicScore;
-    private Integer activityScore;
-
-    public static CrewScoreResponseDto fromEntity(final Crew crew) {
-        return CrewScoreResponseDto.builder()
-                .activityScore(crew.getActivityScore())
-                .basicScore(crew.getBasicScore())
-                .build();
-    }
+    private Float basicScore;
+    private Float activityScore;
 }
