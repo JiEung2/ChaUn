dd0020b7dbd40cfa20ee0e9bfa129496d4a48ac5
송준혁 <fprhqkrtk303@naver.com>
2024-09-25 22:21:08 +0900

    feat: 추가 체형 예측 정보 엔티티

---
+++ project/back/health/src/main/java/com/ssafy/health/domain/body/BodyPredict/entity/BodyExtraPrediction.java
@@ -0,0 +1,31 @@
+package com.ssafy.health.domain.body.BodyPredict.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@Document(collection = "predict_extra")
+public class BodyExtraPrediction {
+
+    @Field("user_id")
+    private Long userId;
+    private Float p30;
+    private Float p90;
+    @Field("create_at")
+    private LocalDateTime createdAt;
+    private ExerciseDetail exercise;
+
+    @Getter
+    @AllArgsConstructor
+    public static class ExerciseDetail {
+        @Field("exercise_id")
+        private Long exerciseId;
+        private Integer count;
+        private Integer duration;
+    }
+}
