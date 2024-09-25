ec34ab53857833786af609490f764e1d18225dbb
송준혁 <fprhqkrtk303@naver.com>
2024-09-25 17:31:01 +0900

    fix: DTO에서 불필요한 @Field 어노테이션 삭제

--- project/back/health/src/main/java/com/ssafy/health/domain/account/dto/response/RecommendedCrewResponseDto.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/account/dto/response/RecommendedCrewResponseDto.java
@@ -10,15 +10,12 @@ import java.util.List;
 @Builder
 public class RecommendedCrewResponseDto {
 
-    @Field("user_id")
     private Long userId;
-    @Field("crew_recommend")
     private List<CrewRecommendList> crewRecommend;
 
     @Data
     @Builder
     public static class CrewRecommendList {
-        @Field("crew_id")
         private Long crewId;
         private Float similarity;
     }
