1ec4cd9d06af7225186054847be3bf4e685e56a8
송준혁 <fprhqkrtk303@naver.com>
2024-09-25 22:24:25 +0900

    feat: 추가 체형 예측 조회 API

--- project/back/health/src/main/java/com/ssafy/health/domain/body/BodyPredict/controller/BodyPredictController.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/body/BodyPredict/controller/BodyPredictController.java
@@ -2,6 +2,7 @@ package com.ssafy.health.domain.body.BodyPredict.controller;
 
 import com.ssafy.health.common.ApiResponse;
 import com.ssafy.health.domain.body.BodyPredict.dto.response.BasicPredictionResponseDto;
+import com.ssafy.health.domain.body.BodyPredict.dto.response.ExtraPredictionResponseDto;
 import com.ssafy.health.domain.body.BodyPredict.service.BodyPredictReadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,5 +19,10 @@ public class BodyPredictController implements BodyPredictControllerApi {
     @GetMapping("/basic")
     public ApiResponse<BasicPredictionResponseDto> getBasicPrediction() {
         return ApiResponse.success(bodyPredictReadService.getBasicPrediction());
+    }
+
+    @GetMapping("/extra")
+    public ApiResponse<ExtraPredictionResponseDto> getExtraPrediction() {
+        return ApiResponse.success(bodyPredictReadService.getExtraPrediction());
     }
 }
--- project/back/health/src/main/java/com/ssafy/health/domain/body/BodyPredict/service/BodyPredictReadService.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/body/BodyPredict/service/BodyPredictReadService.java
@@ -2,7 +2,9 @@ package com.ssafy.health.domain.body.BodyPredict.service;
 
 import com.ssafy.health.common.security.SecurityUtil;
 import com.ssafy.health.domain.body.BodyPredict.dto.response.BasicPredictionResponseDto;
+import com.ssafy.health.domain.body.BodyPredict.dto.response.ExtraPredictionResponseDto;
 import com.ssafy.health.domain.body.BodyPredict.entity.BodyBasicPrediction;
+import com.ssafy.health.domain.body.BodyPredict.entity.BodyExtraPrediction;
 import com.ssafy.health.domain.body.BodyPredict.repository.BodyBasicPredictRepository;
 import com.ssafy.health.domain.body.BodyPredict.repository.BodyExtraPredictRepository;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +28,25 @@ public class BodyPredictReadService {
                 .p30(basicPrediction.getP30())
                 .p90(basicPrediction.getP90())
                 .createdAt(basicPrediction.getCreatedAt())
+                .build();
+    }
+
+    public ExtraPredictionResponseDto getExtraPrediction() {
+
+        Long userId = SecurityUtil.getCurrentUserId();
+        BodyExtraPrediction extraPrediction = extraRepository.findFirstByUserIdOrderByCreatedAtDesc(userId)
+                .orElseThrow();
+
+        return ExtraPredictionResponseDto.builder()
+                .userId(extraPrediction.getUserId())
+                .p30(extraPrediction.getP30())
+                .p90(extraPrediction.getP90())
+                .exercise(ExtraPredictionResponseDto.ExerciseDetail.builder()
+                        .exerciseId(extraPrediction.getExercise().getExerciseId())
+                        .duration(extraPrediction.getExercise().getDuration())
+                        .count(extraPrediction.getExercise().getCount())
+                        .build())
+                .createdAt(extraPrediction.getCreatedAt())
                 .build();
     }
 }
