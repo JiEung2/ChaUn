447e8c44a9b02063aa62b90d29c2096b20868765
김민주 <kminju2000@naver.com>
2024-09-26 12:09:33 +0900

    chore: tanstack query setting

--- project/front/health/src/App.tsx
+++ project/front/health/src/App.tsx
@@ -17,31 +17,36 @@ import Mypage from './pages/Mypage/Mypage';
 import Profile from './pages/Profile/Profile';
 import MyCrew from './pages/Crew/MyCrew/MyCrew';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route element={<CommonLayout />}>
-            <Route path="/" element={<Login />} />
-            <Route path="/survey" element={<Survey />} />
-            <Route path="/surveyResult" element={<SurveyResult />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/home/calendar" element={<Calendar />} />
-            <Route path="/home/quest" element={<Quest />} />
-            <Route path="/exercise/recommend" element={<ExerciseRecommend />} />
-            <Route path="/exercise" element={<Exercise />} />
-            <Route path="/record" element={<Record />} />
-            <Route path="/record/bodyDetail" element={<BodyDetail />} />
-            <Route path="/crew" element={<Crew />} />
-            <Route path="/crew/recommend" element={<CrewRecommend />} />
-            <Route path="/mypage" element={<Mypage />} />
-            <Route path="/alarm" element={<Alarm />} />
-            <Route path="/profile/:userId" element={<Profile />} />
-            <Route path="/crew/mycrew/" element={<MyCrew />} />
-          </Route>
-        </Routes>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <Routes>
+            <Route element={<CommonLayout />}>
+              <Route path="/" element={<Login />} />
+              <Route path="/survey" element={<Survey />} />
+              <Route path="/surveyResult" element={<SurveyResult />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/home/calendar" element={<Calendar />} />
+              <Route path="/home/quest" element={<Quest />} />
+              <Route path="/exercise/recommend" element={<ExerciseRecommend />} />
+              <Route path="/exercise" element={<Exercise />} />
+              <Route path="/record" element={<Record />} />
+              <Route path="/record/bodyDetail" element={<BodyDetail />} />
+              <Route path="/crew" element={<Crew />} />
+              <Route path="/crew/recommend" element={<CrewRecommend />} />
+              <Route path="/mypage" element={<Mypage />} />
+              <Route path="/alarm" element={<Alarm />} />
+              <Route path="/profile/:userId" element={<Profile />} />
+              <Route path="/crew/mycrew/" element={<MyCrew />} />
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
