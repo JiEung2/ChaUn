75287e1581e3611ce07bbbfeaf8af885fa89fed3
JiEung2 <wldmd5007@naver.com>
2024-09-26 14:51:04 +0900

    fix: BattleNotFoundException 수정
    - return 값이 UserException을 참조하고 있던 오류 수정

--- project/back/health/src/main/java/com/ssafy/health/domain/battle/exception/BattleNotFoundException.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/battle/exception/BattleNotFoundException.java
@@ -1,14 +1,12 @@
 package com.ssafy.health.domain.battle.exception;
 
-import com.ssafy.health.domain.account.exception.UserExceptionMessage;
-
 public class BattleNotFoundException extends RuntimeException{
 
     @Override
     public String getMessage() {
-        return UserExceptionMessage.USER_NOT_FOUND.getMessage();
+        return BattleExceptionMessage.BATTLE_NOT_FOUND.getMessage();
     }
     public int getStatus() {
-        return UserExceptionMessage.USER_NOT_FOUND.getStatus();
+        return BattleExceptionMessage.BATTLE_NOT_FOUND.getStatus();
     }
 }
