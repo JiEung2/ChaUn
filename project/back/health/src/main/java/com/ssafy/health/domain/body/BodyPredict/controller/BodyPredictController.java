08f6c756be5e2830979029f70aa53b34b55f2ae6
송준혁 <fprhqkrtk303@naver.com>
2024-09-26 21:16:07 +0900

    feat: 추가 운동을 고려한 체형 예측 요청 API
    - 추가적인 운동을 고려했을 때의 체형 예측을 요청
    - 추가 운동 정보는 2개까지만 전송

--- project/back/health/src/main/java/com/ssafy/health/domain/body/BodyPredict/controller/BodyPredictController.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/body/BodyPredict/controller/BodyPredictController.java
@@ -1,13 +1,14 @@
 package com.ssafy.health.domain.body.BodyPredict.controller;
 
 import com.ssafy.health.common.ApiResponse;
+import com.ssafy.health.domain.body.BodyPredict.dto.ExerciseDetailDto;
+import com.ssafy.health.domain.body.BodyPredict.dto.request.AnalysisRequestDto;
 import com.ssafy.health.domain.body.BodyPredict.dto.response.BasicPredictionResponseDto;
 import com.ssafy.health.domain.body.BodyPredict.dto.response.ExtraPredictionResponseDto;
 import com.ssafy.health.domain.body.BodyPredict.service.BodyPredictReadService;
+import com.ssafy.health.domain.body.BodyPredict.service.BodyPredictWriteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users/predict")
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BodyPredictController implements BodyPredictControllerApi {
 
     private final BodyPredictReadService bodyPredictReadService;
+    private final BodyPredictWriteService bodyPredictWriteService;
 
     @GetMapping("/basic")
     public ApiResponse<BasicPredictionResponseDto> getBasicPrediction() {
@@ -24,5 +26,11 @@ public class BodyPredictController implements BodyPredictControllerApi {
     @GetMapping("/extra")
     public ApiResponse<ExtraPredictionResponseDto> getExtraPrediction() {
         return ApiResponse.success(bodyPredictReadService.getExtraPrediction());
+    }
+
+    @PostMapping("/request-extra")
+    public ApiResponse<AnalysisRequestDto> requestExtraAnalysis(@RequestBody ExerciseDetailDto dto) {
+        return ApiResponse.success(
+                bodyPredictWriteService.requestExtraAnalysis(dto), "추가 체형 예측을 요청하였습니다.");
     }
 }
