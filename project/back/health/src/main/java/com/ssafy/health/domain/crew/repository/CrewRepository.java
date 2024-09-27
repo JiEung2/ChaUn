4518f44ddf89168d1b2d0eaf769de78e5ebd85a9
JiEung2 <wldmd5007@naver.com>
2024-09-27 16:46:41 +0900

    feat: updateCrewBasicScoreByUserAndExercise 쿼리 추가
    - User와 Exercise를 통해 관련된 crew의 basicScore를 업데이트 해주는 쿼리

--- project/back/health/src/main/java/com/ssafy/health/domain/crew/repository/CrewRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/crew/repository/CrewRepository.java
@@ -1,10 +1,13 @@
 package com.ssafy.health.domain.crew.repository;
 
+import com.ssafy.health.domain.account.entity.User;
 import com.ssafy.health.domain.crew.entity.Crew;
+import com.ssafy.health.domain.exercise.entity.Exercise;
 import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -30,4 +33,14 @@ public interface CrewRepository extends JpaRepository<Crew, Long> {
 
     @Query("SELECT c FROM Crew c JOIN FETCH c.exercise WHERE c.id = :crewId")
     Optional<Crew> findCrewWithExerciseById(Long crewId);
+
+    @Modifying
+    @Query("UPDATE Crew c " +
+            "SET c.basicScore = c.basicScore + :basicScore " +
+            "WHERE c IN (" +
+            "    SELECT uc.crew FROM UserCrew uc " +
+            "    WHERE uc.user = :user " +
+            "    AND uc.crew.exercise = :exercise" +
+            ")")
+    void updateCrewBasicScoreByUserAndExercise(User user, Exercise exercise, Float basicScore);
 }
