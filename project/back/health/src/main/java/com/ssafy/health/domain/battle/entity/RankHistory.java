253f5638a6e0c432bf389e32745e570796d72f7f
JiEung2 <181757@jnu.ac.kr>
2024-09-29 02:29:56 +0900

    fix: rank history 수정
    - score를 basicScore와 activityScore로 분리
    - endDate를 LocalDate으로 수정
    - builder 수정
    - 파일 위치 변경

--- project/back/health/src/main/java/com/ssafy/health/domain/battle/entity/RankHistory.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/battle/entity/RankHistory.java
@@ -1,4 +1,4 @@
-package com.ssafy.health.domain.crew.rank.entity;
+package com.ssafy.health.domain.battle.entity;
 
 import com.ssafy.health.common.entity.BaseEntity;
 import com.ssafy.health.domain.account.entity.User;
@@ -9,7 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,9 +24,10 @@ public class RankHistory extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Float score;
+    private Float basicScore;
+    private Float activityScore;
     private Integer ranking;
-    private LocalDateTime endDate;
+    private LocalDate endDate;
 
     @ManyToOne
     @JoinColumn(name = "crew_id")
@@ -37,8 +38,13 @@ public class RankHistory extends BaseEntity {
     private User user;
 
     @Builder
-    public RankHistory(User user, Crew crew) {
-        this.user = user;
+    public RankHistory(Crew crew, User user, Integer ranking, LocalDate endDate, Float basicScore,
+                       Float activityScore) {
         this.crew = crew;
+        this.user = user;
+        this.ranking = ranking;
+        this.endDate = endDate;
+        this.basicScore = basicScore;
+        this.activityScore = activityScore;
     }
 }
