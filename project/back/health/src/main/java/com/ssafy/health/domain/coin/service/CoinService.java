142540e3e6a2ea5ba9a3ec9bc44c930fc0168c40
JiEung2 <181757@jnu.ac.kr>
2024-09-29 02:30:59 +0900

    feat: distributeBattleRewards 기능 추가
    - 배틀 리워드를 제공하는 기능
    - 1, 2, 3등만 제공

--- project/back/health/src/main/java/com/ssafy/health/domain/coin/service/CoinService.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/coin/service/CoinService.java
@@ -1,11 +1,11 @@
 package com.ssafy.health.domain.coin.service;
 
+import static com.ssafy.health.domain.coin.CoinCost.*;
+
 import com.ssafy.health.domain.account.entity.User;
-import com.ssafy.health.domain.account.exception.UserNotFoundException;
-import com.ssafy.health.domain.account.repository.UserRepository;
 import com.ssafy.health.domain.crew.entity.Crew;
-import com.ssafy.health.domain.crew.exception.CrewNotFoundException;
-import com.ssafy.health.domain.crew.repository.CrewRepository;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,5 +24,27 @@ public class CoinService {
 
     public void spendCrewCoins(final Crew crew, final Integer coins) {;
         crew.decreaseCoin(coins);
+    }
+
+    public void distributeBattleRewards(List<User> crewMemberRanking) {
+        Map<Integer, Integer> rewardMap = Map.of(
+                1, FIRST_PLACE_REWARD.getAmount(),
+                2, SECOND_PLACE_REWARD.getAmount(),
+                3, THIRD_PLACE_REWARD.getAmount()
+        );
+
+        crewMemberRanking.stream()
+                .limit(3)
+                .forEach(user -> {
+                    int ranking = crewMemberRanking.indexOf(user) + 1;
+                    Integer coinAmount = rewardMap.get(ranking);
+                    if (coinAmount != null) {
+                        grantCoins(user, coinAmount);
+                    }
+                });
+    }
+
+    private void grantCoins(User user, int coinAmount) {
+        user.increaseCoin(coinAmount);
     }
 }
