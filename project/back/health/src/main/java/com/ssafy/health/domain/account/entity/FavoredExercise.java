5d3c24a78f91b0bb4a28f9d54c1ef69ad4172f22
JiEung2 <181757@jnu.ac.kr>
2024-09-28 16:12:24 +0900

    feat: FavoredExercise 엔티티 생성

---
+++ project/back/health/src/main/java/com/ssafy/health/domain/account/entity/FavoredExercise.java
@@ -0,0 +1,34 @@
+package com.ssafy.health.domain.account.entity;
+
+import com.ssafy.health.domain.exercise.entity.Exercise;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FavoredExercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "exercise_id")
+    private Exercise exercise;
+}
