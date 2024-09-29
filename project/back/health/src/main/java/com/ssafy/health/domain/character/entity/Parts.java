f444ff127c46af1cb6edcc7a91e6f3e5ceb37475
JiEung2 <181757@jnu.ac.kr>
2024-09-29 23:13:20 +0900

    feat: Parts Builder 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/character/entity/Parts.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/character/entity/Parts.java
@@ -5,10 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Parts extends BaseEntity {
 
     @Id
@@ -19,4 +23,12 @@ public class Parts extends BaseEntity {
     private PartsType partsType;
     private Integer cost;
     private String partsImage;
+
+    @Builder
+    public Parts(String name, PartsType partsType, Integer cost, String partsImage) {
+        this.name = name;
+        this.partsType = partsType;
+        this.cost = cost;
+        this.partsImage = partsImage;
+    }
 }
