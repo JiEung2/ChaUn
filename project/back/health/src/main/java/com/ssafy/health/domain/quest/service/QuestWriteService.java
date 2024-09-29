ed17a53b36cc52c743e7b9c4a6a449b3b565882f
송준혁 <fprhqkrtk303@naver.com>
2024-09-30 00:42:46 +0900

    feat: 일일 퀘스트 생성 시 크루, 유저를 나누어 생성

--- project/back/health/src/main/java/com/ssafy/health/domain/quest/service/QuestWriteService.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/quest/service/QuestWriteService.java
@@ -2,11 +2,11 @@ package com.ssafy.health.domain.quest.service;
 
 import com.ssafy.health.domain.account.entity.User;
 import com.ssafy.health.domain.account.repository.UserRepository;
+import com.ssafy.health.domain.crew.entity.Crew;
+import com.ssafy.health.domain.crew.repository.CrewRepository;
 import com.ssafy.health.domain.quest.dto.request.QuestCreateRequestDto;
-import com.ssafy.health.domain.quest.entity.Quest;
-import com.ssafy.health.domain.quest.entity.QuestPeriod;
-import com.ssafy.health.domain.quest.entity.QuestType;
-import com.ssafy.health.domain.quest.entity.UserQuest;
+import com.ssafy.health.domain.quest.entity.*;
+import com.ssafy.health.domain.quest.repository.CrewQuestRepository;
 import com.ssafy.health.domain.quest.repository.QuestRepository;
 import com.ssafy.health.domain.quest.repository.UserQuestRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,8 +19,10 @@ import java.util.List;
 public class QuestWriteService {
 
     private final UserRepository userRepository;
+    private final CrewRepository crewRepository;
     private final QuestRepository questRepository;
     private final UserQuestRepository userQuestRepository;
+    private final CrewQuestRepository crewQuestRepository;
 
     // 퀘스트 생성
     public Quest createQuest(QuestCreateRequestDto requestDto) {
@@ -34,15 +36,23 @@ public class QuestWriteService {
     // TODO: 스프링 스케줄러 사용하여 일별, 월별 퀘스트 생성
     // TODO: 주기별 퀘스트 생성 시 해당 주기에 이미 퀘스트가 생성되어 있다면 예외 처리
     public void createDailyQuest() {
-        List<Quest> questList = questRepository.findAllByPeriod(QuestPeriod.DAILY);
+        List<Quest> dailyQuestList = questRepository.findAllByPeriod(QuestPeriod.DAILY);
         List<User> userList = userRepository.findAll();
+        List<Crew> crewList = crewRepository.findAll();
 
-        // 유저 퀘스트 생성
-        questList.forEach(quest ->
+        dailyQuestList.forEach(quest -> {
+            if (quest.getType().equals(QuestType.CREW)) {
                 userList.forEach(user -> userQuestRepository.save(UserQuest.builder()
                         .quest(quest)
                         .user(user)
-                        .build())));
+                        .build()));
+            } else if (quest.getType().equals(QuestType.INDIVIDUAL)) {
+                crewList.forEach(crew -> crewQuestRepository.save(CrewQuest.builder()
+                        .quest(quest)
+                        .crew(crew)
+                        .build()));
+            }
+        });
     }
 
     public void createMonthlyQuest() {
