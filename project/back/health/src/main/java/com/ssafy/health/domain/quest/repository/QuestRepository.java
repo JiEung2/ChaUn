a81d97c99e7b6385cf9fe34439a2c1f2d3fc1f6a
송준혁 <fprhqkrtk303@naver.com>
2024-09-30 00:26:19 +0900

    feat: Quest 조회 메서드 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/quest/repository/QuestRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/quest/repository/QuestRepository.java
@@ -1,7 +1,17 @@
 package com.ssafy.health.domain.quest.repository;
 
 import com.ssafy.health.domain.quest.entity.Quest;
+import com.ssafy.health.domain.quest.entity.QuestPeriod;
+import com.ssafy.health.domain.quest.entity.QuestType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestRepository extends JpaRepository<Quest, Long> {
+
+    List<Quest> findAllByPeriod(QuestPeriod period);
+
+    List<Quest> findAllByType(QuestType type);
+
+    List<Quest> findAllByTypeAndPeriod(QuestType type, QuestPeriod period);
 }
