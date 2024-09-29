ecb0aaeee2c0e5d9f7d04e8869251444377c3f94
JiEung2 <181757@jnu.ac.kr>
2024-09-29 15:54:58 +0900

    feat: 스케줄러 추가
    - 매주 월요일 새벽 4시 반에 배틀이 끝나도록 설정

--- project/back/health/src/main/java/com/ssafy/health/domain/battle/service/BattleWriteService.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/battle/service/BattleWriteService.java
@@ -16,6 +16,7 @@ import com.ssafy.health.domain.crew.entity.Crew;
 import com.ssafy.health.domain.crew.exception.CrewNotFoundException;
 import com.ssafy.health.domain.crew.repository.CrewRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -67,6 +68,7 @@ public class BattleWriteService {
                 .build();
     }
 
+    @Scheduled(cron = "0 30 4 ? * MON")
     public void finishBattles() {
         List<Battle> ongoingBattles = battleRepository.findByStatus(BattleStatus.STARTED);
 
