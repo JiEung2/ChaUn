4514d2c12a5d6d28a8ead5e7329587c5bf93947b
송준혁 <fprhqkrtk303@naver.com>
2024-09-30 02:19:11 +0900

    feat: 유저, 크루별 퀘스트 조회 API

--- project/back/health/src/main/java/com/ssafy/health/domain/quest/controller/QuestController.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/quest/controller/QuestController.java
@@ -2,13 +2,15 @@ package com.ssafy.health.domain.quest.controller;
 
 import com.ssafy.health.common.ApiResponse;
 import com.ssafy.health.domain.quest.dto.request.QuestCreateRequestDto;
+import com.ssafy.health.domain.quest.dto.response.QuestResponseDto;
 import com.ssafy.health.domain.quest.entity.Quest;
+import com.ssafy.health.domain.quest.entity.QuestType;
+import com.ssafy.health.domain.quest.service.QuestReadService;
 import com.ssafy.health.domain.quest.service.QuestWriteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,11 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuestController {
 
     private final QuestWriteService questWriteService;
+    private final QuestReadService questReadService;
 
     // 퀘스트 신규 생성
     // 유저/크루에게 퀘스트 부여가 아닌, 새로운 퀘스트 종류 생성
     @PostMapping("/create")
     public ApiResponse<Quest> createQuest(@RequestBody QuestCreateRequestDto requestDto) {
         return ApiResponse.success(questWriteService.createQuest(requestDto));
+    }
+
+    @GetMapping("/get/user")
+    public ApiResponse<List<QuestResponseDto>> getUserQuestList() {
+        return ApiResponse.success(questReadService.getAchievableQuest(QuestType.INDIVIDUAL, 0L));
+    }
+
+    @GetMapping("/get/crew")
+    public ApiResponse<List<QuestResponseDto>> getCrewQuestList(@RequestParam Long crewId) {
+        return ApiResponse.success(questReadService.getAchievableQuest(QuestType.CREW, crewId));
     }
 }
