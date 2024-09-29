78aa177042d3a4c49c64cd109138587414034d4e
JiEung2 <181757@jnu.ac.kr>
2024-09-29 15:54:17 +0900

    feat: 스케줄러를 사용하기 위한 어노테이션 추가

--- project/back/health/src/main/java/com/ssafy/health/HealthApplication.java
+++ project/back/health/src/main/java/com/ssafy/health/HealthApplication.java
@@ -7,10 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableMongoAuditing
+@EnableScheduling
 public class HealthApplication {
 
 	@PostConstruct
