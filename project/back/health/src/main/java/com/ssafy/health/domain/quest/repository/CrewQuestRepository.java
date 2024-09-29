1001fb5621938abb54ca58cf8880745639357689
송준혁 <fprhqkrtk303@naver.com>
2024-09-30 01:37:21 +0900

    feat: 상태별 퀘스트 JPA 조회 메서드 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/quest/repository/CrewQuestRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/quest/repository/CrewQuestRepository.java
@@ -1,7 +1,13 @@
 package com.ssafy.health.domain.quest.repository;
 
+import com.ssafy.health.domain.crew.entity.Crew;
 import com.ssafy.health.domain.quest.entity.CrewQuest;
+import com.ssafy.health.domain.quest.entity.QuestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CrewQuestRepository extends JpaRepository<CrewQuest, Long> {
+
+    List<CrewQuest> findAllByCrewAndStatus(Crew crew, QuestStatus status);
 }
--- project/back/health/src/main/java/com/ssafy/health/domain/quest/repository/UserQuestRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/quest/repository/UserQuestRepository.java
@@ -1,7 +1,13 @@
 package com.ssafy.health.domain.quest.repository;
 
+import com.ssafy.health.domain.account.entity.User;
+import com.ssafy.health.domain.quest.entity.QuestStatus;
 import com.ssafy.health.domain.quest.entity.UserQuest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserQuestRepository extends JpaRepository<UserQuest, Long> {
+
+    List<UserQuest> findAllByUserAndStatus(User user, QuestStatus status);
 }
