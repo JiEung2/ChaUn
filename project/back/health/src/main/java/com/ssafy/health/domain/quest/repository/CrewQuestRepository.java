c3b193ecb316e2d33ce70fca64940cbba1e1b5d3
송준혁 <fprhqkrtk303@naver.com>
2024-09-30 01:58:27 +0900

    refactor: 퀘스트 조회 메서드에 퀘스트 상태 선택 추가
    퀘스트 상태 Enum에 따라 조회하고자 하는 퀘스트 상태 선택

--- project/back/health/src/main/java/com/ssafy/health/domain/quest/repository/CrewQuestRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/quest/repository/CrewQuestRepository.java
@@ -4,10 +4,12 @@ import com.ssafy.health.domain.crew.entity.Crew;
 import com.ssafy.health.domain.quest.entity.CrewQuest;
 import com.ssafy.health.domain.quest.entity.QuestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CrewQuestRepository extends JpaRepository<CrewQuest, Long> {
 
-    List<CrewQuest> findAllByCrewAndStatus(Crew crew, QuestStatus status);
+    @Query("SELECT q FROM CrewQuest q WHERE q.status IN :status")
+    List<CrewQuest> findAllByCrewAndStatus(Crew crew, List<QuestStatus> status);
 }
--- project/back/health/src/main/java/com/ssafy/health/domain/quest/repository/UserQuestRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/quest/repository/UserQuestRepository.java
@@ -4,10 +4,12 @@ import com.ssafy.health.domain.account.entity.User;
 import com.ssafy.health.domain.quest.entity.QuestStatus;
 import com.ssafy.health.domain.quest.entity.UserQuest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface UserQuestRepository extends JpaRepository<UserQuest, Long> {
 
-    List<UserQuest> findAllByUserAndStatus(User user, QuestStatus status);
+    @Query("SELECT q FROM UserQuest q WHERE q.status IN :status")
+    List<UserQuest> findAllByUserAndStatus(User user, List<QuestStatus> status);
 }
