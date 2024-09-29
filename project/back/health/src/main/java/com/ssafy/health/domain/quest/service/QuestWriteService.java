e08f0c611769209a36f3917b443a7050712ccfdd
송준혁 <fprhqkrtk303@naver.com>
2024-09-30 00:26:55 +0900

    feat: 유저별 일일 퀘스트 생성 메서드

--- project/back/health/src/main/java/com/ssafy/health/domain/quest/service/QuestWriteService.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/quest/service/QuestWriteService.java
@@ -1,18 +1,26 @@
 package com.ssafy.health.domain.quest.service;
 
+import com.ssafy.health.domain.account.entity.User;
+import com.ssafy.health.domain.account.repository.UserRepository;
 import com.ssafy.health.domain.quest.dto.request.QuestCreateRequestDto;
 import com.ssafy.health.domain.quest.entity.Quest;
 import com.ssafy.health.domain.quest.entity.QuestPeriod;
 import com.ssafy.health.domain.quest.entity.QuestType;
+import com.ssafy.health.domain.quest.entity.UserQuest;
 import com.ssafy.health.domain.quest.repository.QuestRepository;
+import com.ssafy.health.domain.quest.repository.UserQuestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class QuestWriteService {
 
+    private final UserRepository userRepository;
     private final QuestRepository questRepository;
+    private final UserQuestRepository userQuestRepository;
 
     // 퀘스트 생성
     public Quest createQuest(QuestCreateRequestDto requestDto) {
@@ -21,6 +29,19 @@ public class QuestWriteService {
                 requestDto.getTitle(),
                 requestDto.getQuestPeriod(),
                 requestDto.getCoins()));
+    }
+
+    // TODO: 스프링 스케줄러 사용하여 일별, 월별 퀘스트 생성
+    public void createDailyQuest() {
+        List<Quest> questList = questRepository.findAllByPeriod(QuestPeriod.DAILY);
+        List<User> userList = userRepository.findAll();
+
+        // 유저 퀘스트 생성
+        questList.forEach(quest ->
+                userList.forEach(user -> userQuestRepository.save(UserQuest.builder()
+                        .quest(quest)
+                        .user(user)
+                        .build())));
     }
 
     private Quest questBuilder(QuestType type, String title, QuestPeriod period, Integer coins) {
