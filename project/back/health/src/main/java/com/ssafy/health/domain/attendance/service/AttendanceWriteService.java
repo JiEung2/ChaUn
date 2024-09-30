7fae1740a72ebc0b9d9ab0c7c2c159702c5dd304
송준혁 <fprhqkrtk303@naver.com>
2024-09-30 17:04:46 +0900

    feat: 2주 이상 출석 시 퀘스트 완료 처리

--- project/back/health/src/main/java/com/ssafy/health/domain/attendance/service/AttendanceWriteService.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/attendance/service/AttendanceWriteService.java
@@ -8,9 +8,16 @@ import com.ssafy.health.domain.attendance.dto.response.AttendanceSuccessDto;
 import com.ssafy.health.domain.attendance.entity.Attendance;
 import com.ssafy.health.domain.attendance.repository.AttendanceRepository;
 import com.ssafy.health.domain.coin.service.CoinService;
+import com.ssafy.health.domain.quest.entity.QuestStatus;
+import com.ssafy.health.domain.quest.entity.UserQuest;
+import com.ssafy.health.domain.quest.repository.UserQuestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.YearMonth;
 
 @Service
 @Transactional
@@ -21,14 +28,32 @@ public class AttendanceWriteService {
     private final UserRepository userRepository;
     private final AttendanceValidator attendanceValidator;
     private final AttendanceRepository attendanceRepository;
+    private final UserQuestRepository userQuestRepository;
 
     public AttendanceSuccessDto markAttendance() {
+
+        YearMonth yearMonth = YearMonth.from(LocalDateTime.now());
+        LocalDateTime startOfMonth = yearMonth.atDay(1).atStartOfDay();
+        LocalDateTime endOfMonth = yearMonth.atEndOfMonth().atTime(LocalTime.MAX);
+
         attendanceValidator.validateTodayAttendance();
 
         User user = findUserById(SecurityUtil.getCurrentUserId());
         attendanceRepository.save(Attendance.builder().user(user).build());
 
         coinService.giveAttendanceReward(user);
+
+        // 출석 퀘스트 확인
+        int attendCount = attendanceRepository
+                .findByUserIdAndCreatedAtBetween(user.getId(), startOfMonth, endOfMonth).size();
+
+        if (attendCount >= 14) {
+            UserQuest quest = userQuestRepository.findUserQuestWithCriteria(
+                    user, QuestStatus.CREATED, "2주 이상 출석");
+
+            // TODO: 퀘스트 완료 알림 전송
+            quest.updateStatus(QuestStatus.COMPLETED);
+        }
 
         return new AttendanceSuccessDto();
     }
