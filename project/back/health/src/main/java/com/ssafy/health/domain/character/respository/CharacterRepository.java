c6da1e72444fcb6e9d0117fcdaa0b6b4ae932748
JiEung2 <181757@jnu.ac.kr>
2024-09-30 22:50:55 +0900

    feat: findByBodyTypeIdAndGender 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/character/respository/CharacterRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/character/respository/CharacterRepository.java
@@ -1,7 +1,10 @@
 package com.ssafy.health.domain.character.respository;
 
+import com.ssafy.health.domain.account.entity.Gender;
 import com.ssafy.health.domain.character.entity.Character;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CharacterRepository extends JpaRepository<Character, Long> {
+    Optional<Character> findByBodyTypeIdAndGender(Long bodyTypeId, Gender gender);
 }
