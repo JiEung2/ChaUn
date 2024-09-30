876a8c5741e090083cdb439c8bd3cc5e64d015a0
JiEung2 <181757@jnu.ac.kr>
2024-09-30 23:27:46 +0900

    feat: character를 반환해주는 컨트롤러 생성

--- project/back/health/src/main/java/com/ssafy/health/domain/character/controller/CharacterController.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/character/controller/CharacterController.java
@@ -3,12 +3,16 @@ package com.ssafy.health.domain.character.controller;
 import com.ssafy.health.common.ApiResponse;
 import com.ssafy.health.domain.character.dto.request.CharacterSaveRequestDto;
 import com.ssafy.health.domain.character.dto.request.PartsSaveRequestDto;
+import com.ssafy.health.domain.character.dto.response.CharacterResponseDto;
 import com.ssafy.health.domain.character.dto.response.CharacterSaveSuccessDto;
 import com.ssafy.health.domain.character.dto.response.PartsSaveSuccessDto;
+import com.ssafy.health.domain.character.service.CharacterReadService;
 import com.ssafy.health.domain.character.service.CharacterWriteService;
 import jakarta.validation.Valid;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -20,6 +24,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class CharacterController {
 
+    private final CharacterReadService characterReadService;
     private final CharacterWriteService characterWriteService;
 
     @PostMapping("/character")
@@ -36,5 +41,15 @@ public class CharacterController {
             @Valid @RequestPart("partsSaveRequestDto") PartsSaveRequestDto partsSaveRequestDto,
             @RequestPart("partImage") MultipartFile partImage) throws IOException {
         return ApiResponse.success(characterWriteService.saveParts(partsSaveRequestDto, partImage));
+    }
+
+    @GetMapping("/users/my/character")
+    public ApiResponse<CharacterResponseDto> getMyCharacter() {
+        return ApiResponse.success(characterReadService.getMyCharacter());
+    }
+
+    @GetMapping("/users/{user_id}/character")
+    public ApiResponse<CharacterResponseDto> getCharacter(@PathVariable("user_id") Long userId) {
+        return ApiResponse.success(characterReadService.getCharacter(userId));
     }
 }
