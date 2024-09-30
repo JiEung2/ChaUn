09fdbd575a9369488f3aacc997aae1187254e874
JiEung2 <181757@jnu.ac.kr>
2024-09-30 22:51:16 +0900

    feat: CharacterSet 빌더 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/character/entity/CharacterSet.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/character/entity/CharacterSet.java
@@ -6,12 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CharacterSet {
 
     @Id
@@ -22,11 +25,18 @@ public class CharacterSet {
     @JoinColumn(name = "characters_id")
     private Character character;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "parts_id")
     private Parts parts;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @Builder
+    public CharacterSet(User user, Character character, Parts parts) {
+        this.user = user;
+        this.character = character;
+        this.parts = parts;
+    }
 }
