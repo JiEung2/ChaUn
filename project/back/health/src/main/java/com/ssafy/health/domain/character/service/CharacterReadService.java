a72b92c924aad8599d84f47fcb3c7be9156e2ae5
JiEung2 <181757@jnu.ac.kr>
2024-09-30 23:27:28 +0900

    feat: CharacterReadService 추가
    - 나의 캐릭터를 반환하는 메서드
    - 사람들의 캐릭터를 반환하는 메서드
    - 캐릭터 url을 반환하는 메서드

---
+++ project/back/health/src/main/java/com/ssafy/health/domain/character/service/CharacterReadService.java
@@ -0,0 +1,41 @@
+package com.ssafy.health.domain.character.service;
+
+import com.ssafy.health.common.security.SecurityUtil;
+import com.ssafy.health.domain.character.dto.response.CharacterResponseDto;
+import com.ssafy.health.domain.character.entity.Character;
+import com.ssafy.health.domain.character.entity.CharacterSet;
+import com.ssafy.health.domain.character.exception.CharacterNotFoundException;
+import com.ssafy.health.domain.character.exception.CharacterSetNotFoundException;
+import com.ssafy.health.domain.character.respository.CharacterRepository;
+import com.ssafy.health.domain.character.respository.CharacterSetRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CharacterReadService {
+
+    private final CharacterRepository characterRepository;
+    private final CharacterSetRepository characterSetRepository;
+
+    public CharacterResponseDto getMyCharacter() {
+        return CharacterResponseDto.builder()
+                .characterUrl(getCharacterUrl(SecurityUtil.getCurrentUserId())).build();
+    }
+
+    public CharacterResponseDto getCharacter(Long userId) {
+        return CharacterResponseDto.builder()
+                .characterUrl(getCharacterUrl(userId)).build();
+    }
+
+    public String getCharacterUrl(Long userId){
+        CharacterSet characterSet = characterSetRepository.findByUserId(userId).orElseThrow(CharacterSetNotFoundException::new);
+
+        Long id = characterSet.getCharacter().getId() + characterSet.getParts().getId();
+        Character character = characterRepository.findById(id).orElseThrow(CharacterNotFoundException::new);
+
+        return character.getCharacterFile();
+    }
+}
