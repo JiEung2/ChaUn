7bb0d104776c95029eba22f5fe1bf8f7b605cea6
JiEung2 <wldmd5007@naver.com>
2024-09-30 17:40:17 +0900

    Revert "feat: 테스트를 위한 cookie 설정 수정"
    This reverts commit 70a9673d6f7305a0f0f78f7e0938a376e5e464da.

--- project/back/health/src/main/java/com/ssafy/health/common/oauth/service/CookieService.java
+++ project/back/health/src/main/java/com/ssafy/health/common/oauth/service/CookieService.java
@@ -9,9 +9,10 @@ public class CookieService {
 
         Cookie cookie = new Cookie(key, value); //쿠키 생성
         cookie.setMaxAge(60 * 60 * 60); //쿠키가 살아있을 시간 설정
-        cookie.setSecure(true); //https 환경에서만 쿠키가 사용될 수 있게 하는 설정
+        cookie.setSecure(false); //https 환경에서만 쿠키가 사용될 수 있게 하는 설정
         cookie.setPath("/"); //쿠키가 보일 위치는 모든 전역 위치
         cookie.setHttpOnly(true); //js가 쿠키를 가져가지 못하게 설정
+        cookie.setDomain("localhost");
 
         return cookie;
     }
