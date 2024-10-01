3566c5ede5046e7dfbff1b90ce5a21ab3a9ee01b
JiEung2 <181757@jnu.ac.kr>
2024-10-01 18:26:34 +0900

    feat: saveSnapshot swagger 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/character/controller/CharacterControllerApi.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/character/controller/CharacterControllerApi.java
@@ -2,13 +2,17 @@ package com.ssafy.health.domain.character.controller;
 
 import com.ssafy.health.common.ApiResponse;
 import com.ssafy.health.domain.character.dto.response.CharacterResponseDto;
+import com.ssafy.health.domain.character.dto.response.CharacterSnapshotSuccessDto;
 import com.ssafy.health.domain.character.dto.response.PartsListDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.io.IOException;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "캐릭터 컨트롤러", description = "캐릭터, 파츠, 세트를 생성, 조회, 수정하는 회원 캐릭터를 관리하는 클래스")
 public interface CharacterControllerApi {
@@ -106,4 +110,24 @@ public interface CharacterControllerApi {
     })
     ApiResponse<PartsListDto> getParts();
 
+    @Operation(
+            summary = "캐릭터 스냅샷 저장",
+            description = "캐릭터 스냅샷을 저장합니다."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "스냅샷 저장 완료",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(value = """
+                                    {
+                                      "status": 200,
+                                      "message": "",
+                                      "data": {
+                                        "message": "스냅샷을 저장하였습니다."
+                                      }
+                                    }"""
+                            ))
+            ),
+    })
+    ApiResponse<CharacterSnapshotSuccessDto> saveSnapshot(@RequestParam("snapshot") MultipartFile snapshot)
+            throws IOException;
 }
