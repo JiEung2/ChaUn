219d74457c41ec3b949ff94fb846087232bca6ab
JiEung2 <181757@jnu.ac.kr>
2024-10-01 15:16:49 +0900

    feat: 파츠 장착 컨트롤러 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/character/controller/CharacterController.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/character/controller/CharacterController.java
@@ -5,6 +5,7 @@ import com.ssafy.health.domain.character.dto.request.CharacterSaveRequestDto;
 import com.ssafy.health.domain.character.dto.request.PartsSaveRequestDto;
 import com.ssafy.health.domain.character.dto.response.CharacterResponseDto;
 import com.ssafy.health.domain.character.dto.response.CharacterSaveSuccessDto;
+import com.ssafy.health.domain.character.dto.response.PartsListDto;
 import com.ssafy.health.domain.character.dto.response.PartsSaveSuccessDto;
 import com.ssafy.health.domain.character.service.CharacterReadService;
 import com.ssafy.health.domain.character.service.CharacterWriteService;
@@ -12,6 +13,7 @@ import jakarta.validation.Valid;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +24,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
-public class CharacterController {
+public class CharacterController implements CharacterControllerApi{
 
     private final CharacterReadService characterReadService;
     private final CharacterWriteService characterWriteService;
@@ -52,4 +54,10 @@ public class CharacterController {
     public ApiResponse<CharacterResponseDto> getCharacter(@PathVariable("user_id") Long userId) {
         return ApiResponse.success(characterReadService.getCharacter(userId));
     }
+
+    @PatchMapping("/users/character/parts/{parts_id}")
+    public ApiResponse<CharacterResponseDto> applyParts(@PathVariable("parts_id") Long partsId) {
+        return ApiResponse.success(characterWriteService.applyParts(partsId));
+    }
+
 }
