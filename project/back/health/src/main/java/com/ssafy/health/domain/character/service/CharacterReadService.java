29da56d45115232ec0fa23fa51dfbcfb2a834131
JiEung2 <181757@jnu.ac.kr>
2024-10-01 15:17:05 +0900

    feat: 파츠 정보를 가져오는 메서드 구현

--- project/back/health/src/main/java/com/ssafy/health/domain/character/service/CharacterReadService.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/character/service/CharacterReadService.java
@@ -2,12 +2,17 @@ package com.ssafy.health.domain.character.service;
 
 import com.ssafy.health.common.security.SecurityUtil;
 import com.ssafy.health.domain.character.dto.response.CharacterResponseDto;
+import com.ssafy.health.domain.character.dto.response.PartsInfoDto;
+import com.ssafy.health.domain.character.dto.response.PartsListDto;
 import com.ssafy.health.domain.character.entity.Character;
 import com.ssafy.health.domain.character.entity.CharacterSet;
+import com.ssafy.health.domain.character.entity.Parts;
 import com.ssafy.health.domain.character.exception.CharacterNotFoundException;
 import com.ssafy.health.domain.character.exception.CharacterSetNotFoundException;
 import com.ssafy.health.domain.character.respository.CharacterRepository;
 import com.ssafy.health.domain.character.respository.CharacterSetRepository;
+import com.ssafy.health.domain.character.respository.PartsRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class CharacterReadService {
 
+    private final PartsRepository partsRepository;
     private final CharacterRepository characterRepository;
     private final CharacterSetRepository characterSetRepository;
 
@@ -43,4 +49,12 @@ public class CharacterReadService {
 
         return character.getCharacterFile();
     }
+
+    public PartsListDto getParts(){
+        List<Parts> partsList = partsRepository.findAll();
+        List<PartsInfoDto> partsInfoDtoList = partsList.stream().map(PartsInfoDto::fromEntity).toList();
+
+        return PartsListDto.builder().partsList(partsInfoDtoList).build();
+    }
+
 }
