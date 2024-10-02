8d2e271817406316cd24e11b31cda936df993dbb
minyeong981 <rlaalsdud41@naver.com>
2024-10-02 10:17:11 +0900

    test: 로그인 url 실서버 연결

--- project/front/health/src/pages/Login/Login.tsx
+++ project/front/health/src/pages/Login/Login.tsx
@@ -1,22 +1,22 @@
 import './Login.scss'; // SCSS 파일 임포트
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import KakaoButton from '@/components/Button/LoginButton/KakaoButtom';
 import GoogleButton from '@/components/Button/LoginButton/Google';
 import MainGirl from '@/assets/image/maingirl.png';
 import MainBoy from '@/assets/image/mainboy.png';
 
 export default function LoginPage() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const moveKakao = () => {
     // TODO : 카카오 로그인 페이지로 이동
-    // window.location.href = "카카오 로그인 페이지 URL";
+    window.location.href = `${import.meta.env.VITE_APP_BASE_URL_NO_ENDPOINT}`;
     // 임시로 servey 페이지로 이동
-    navigate('/survey');
+    // navigate('/survey');
   };
   const moveGoogle = () => {
     //TODO : 구글 로그인 페이지로 이동
-    // window.location.href = "구글 로그인 페이지 URL";
-    navigate('/survey');
+    window.location.href = `${import.meta.env.VITE_APP_BASE_URL_NO_ENDPOINT}`;
+    // navigate('/survey');
   };
   return (
     <div className="login-container">
