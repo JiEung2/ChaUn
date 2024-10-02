dda9c1ac389ddf95e7213973b5557e29e9c941d2
JiEung2 <181757@jnu.ac.kr>
2024-10-03 01:42:23 +0900

    feat: FavoredRepository에 findByUserId 추가
    - 운동 추천에서 gpt에 좋아하는 운동을 추가하기 위한 조회 쿼리

--- project/back/health/src/main/java/com/ssafy/health/domain/account/repository/FavoredRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/account/repository/FavoredRepository.java
@@ -1,7 +1,9 @@
 package com.ssafy.health.domain.account.repository;
 
 import com.ssafy.health.domain.account.entity.FavoredExercise;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FavoredRepository extends JpaRepository<FavoredExercise, Long> {
+    List<FavoredExercise> findByUserId(Long userId);
 }
