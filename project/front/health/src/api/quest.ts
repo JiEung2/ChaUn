565fed71c4f8577d3784a1cd56f7c865be1d0000
인호현 <inhohyun36@gmail.com>
2024-10-03 16:01:29 +0900

    feat: 개발 모드에 따른 axios 상태 변동

--- project/front/health/src/api/quest.ts
+++ project/front/health/src/api/quest.ts
@@ -1,8 +1,8 @@
-import axiosInstance from './axiosInstance';
+import exportAxios from './axiosInstance';
 
 const baseUrl = import.meta.env.VITE_APP_BASE_URL;
 
 export const getQuest = async () => {
-  const response = await axiosInstance.get(`${baseUrl}/quest/get/user`);
+  const response = await exportAxios.get(`${baseUrl}/quest/get/user`);
   return response.data;
 };
