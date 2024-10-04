dd6055cc7e385567d90e42fb200e91a268bfdec8
minyeong981 <rlaalsdud41@naver.com>
2024-10-04 14:45:22 +0900

    design: coin 컴포넌트 수정

--- project/front/health/src/components/Coin/Coin.tsx
+++ project/front/health/src/components/Coin/Coin.tsx
@@ -4,11 +4,13 @@ import './Coin.scss';
 interface CoinProps {
   amount: number;
   style?: 'basic' | 'styled';
+  width?: string;
+  height?: string;
 }
 
-export default function Coin({ amount, style = 'basic' }: CoinProps) {
+export default function Coin({ amount, style = 'basic', width = 'auto', height = 'auto' }: CoinProps) {
   return (
-    <div className={`coinContainer ${style}`}>
+    <div className={`coinContainer ${style}`} style={{ width, height }}>
       <img src={coinIcon} alt="coin" className="coinIcon" />
       <span className="coinAmount">{amount}</span>
     </div>
