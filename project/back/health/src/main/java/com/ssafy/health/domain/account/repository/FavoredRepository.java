a4954a1f55e6813a3cef732338687c1feb54901c
송준혁 <fprhqkrtk303@naver.com>
2024-10-04 12:16:01 +0900

    feat: 사용자 선호 운동 조회 쿼리

--- project/back/health/src/main/java/com/ssafy/health/domain/account/repository/FavoredRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/account/repository/FavoredRepository.java
@@ -3,5 +3,9 @@ package com.ssafy.health.domain.account.repository;
 import com.ssafy.health.domain.account.entity.FavoredExercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FavoredRepository extends JpaRepository<FavoredExercise, Long> {
+
+    List<FavoredExercise> findAllByUserId(Long userId);
 }
