7715c11c0ab122bfc84577a89dae53e70496d285
JiEung2 <181757@jnu.ac.kr>
2024-10-06 17:37:34 +0900

    fix: CharacterResponseDto 수정
    - 반환 필드에 gender 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/character/dto/response/CharacterResponseDto.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/character/dto/response/CharacterResponseDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.health.domain.character.dto.response;
 
+import com.ssafy.health.domain.account.entity.Gender;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,4 +9,5 @@ import lombok.Getter;
 public class CharacterResponseDto {
     private final String characterUrl;
     private final Long bodyTypeId;
+    private final Gender gender;
 }
