9045e823b55ac43efae7658d162b8c941d7e9a70
송준혁 <fprhqkrtk303@naver.com>
2024-10-06 20:23:00 +0900

    feat: 퀘스트 기간별 조회 후 상태 업데이트 쿼리 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/quest/repository/UserQuestRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/quest/repository/UserQuestRepository.java
@@ -1,9 +1,11 @@
 package com.ssafy.health.domain.quest.repository;
 
 import com.ssafy.health.domain.account.entity.User;
+import com.ssafy.health.domain.quest.entity.QuestPeriod;
 import com.ssafy.health.domain.quest.entity.QuestStatus;
 import com.ssafy.health.domain.quest.entity.UserQuest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -12,6 +14,10 @@ public interface UserQuestRepository extends JpaRepository<UserQuest, Long> {
 
     @Query("SELECT q FROM UserQuest q WHERE q.user = :user AND q.status IN :status")
     List<UserQuest> findAllByUserAndStatus(User user, List<QuestStatus> status);
+
+    @Modifying
+    @Query("UPDATE UserQuest uq SET uq.status = :status WHERE uq.quest.period = :period")
+    void updateAllStatusByPeriod(QuestPeriod period, QuestStatus status);
 
     @Query("""
             SELECT DISTINCT uq FROM UserQuest uq JOIN FETCH uq.quest q
