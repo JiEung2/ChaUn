e42c1dc061aa996253c47e8f4c8030efee6a769f
김민영 <rlaalsdud41@naver.com>
2024-10-06 10:58:57 +0900

    feat: fcm 토큰 발급 성공

--- project/front/health/public/serviceWorkerRegistration.js
+++ project/front/health/public/serviceWorkerRegistration.js
@@ -1,49 +1,17 @@
-// import { getToken } from 'firebase/messaging';
+// const FIREBASE_VAPID_KEY = import.meta.env.VITE_APP_FIREBASE_VAPID_KEY;
+// console.log(FIREBASE_VAPID_KEY);
+const config = {
+  apiKey: 'AIzaSyDevxneNic0RRElDsYDwt0GLGtTJIhttjo',
+  authDomain: 'ssafy-c106.firebaseapp.com',
+  projectId: 'ssafy-c106',
+  storageBucket: 'ssafy-c106.appspot.com',
+  messagingSenderId: '642150988562',
+  appId: '1:642150988562:web:d0a7a1be33256b114b11cb',
+};
 
-// async function registerServiceWorker() {
-//   if ('serviceWorker' in navigator) {
-//     try {
-//       console.log('서비스 워커 등록 시도 중...');
-//       const firebaseRegistration = await navigator.serviceWorker.register('/firebase-messaging-sw.js');
-//       console.log('Firebase Service Worker 등록 성공:', firebaseRegistration.scope);
+firebase.initializeApp(config);
 
-//       console.log('ServiceWorker registration successful with scope: ', firebaseRegistration.scope);
-//       console.log('서비스 워커 활성화');
-
-//       // FCM 토큰이 세션에 없는 경우에만 알림 권한 요청 및 토큰 발급
-//       if (!sessionStorage.getItem('fcmToken')) {
-//         const permission = await Notification.requestPermission();
-//         if (permission === 'granted') {
-//           console.log('알림 권한 허용됨');
-//           const currentToken = await getToken(messaging, {
-//             vapidKey: 'BHwpLNL8I7G3uVXMnYxMZWiopgbTh5k0bC-HRQPEVXj6mz7cPrjcNpgEhwDf08bq99wJF7CQA3DRa16Zb',
-//             serviceWorkerRegistration: firebaseRegistration,
-//           });
-//           if (currentToken) {
-//             sessionStorage.setItem('fcmToken', currentToken);
-//             console.log('FCM 토큰 발급 성공:', currentToken);
-//           } else {
-//             console.warn('FCM 토큰을 가져올 수 없습니다.');
-//           }
-//         } else {
-//           console.log('알림 권한이 거부되었습니다.');
-//         }
-//       } else {
-//         console.log('기존 FCM 토큰이 세션에 저장되어 있습니다.');
-//       }
-//     } catch (error) {
-//       console.error('Service Worker 등록 실패:', error);
-//     }
-//   } else {
-//     console.warn('Service Worker not supported in this browser');
-//   }
-// }
-
-// // 서비스 워커 등록 함수 호출
-// registerServiceWorker();
-console.log('env:', import.meta.env);
-const FIREBASE_VAPID_KEY = import.meta.env.VITE_APP_FIREBASE_VAPID_KEY;
-console.log('VAPID 키:', FIREBASE_VAPID_KEY);
+const messaging = firebase.messaging();
 async function registerServiceWorker() {
   if ('serviceWorker' in navigator) {
     try {
@@ -60,7 +28,7 @@ async function registerServiceWorker() {
         console.log('알림 권한이 거부되었습니다.');
       } else {
         const permission = await Notification.requestPermission();
-        console.log('권한 요청 결과:', permission); // 권한 상태 출력
+        console.log('권한 요청 결과:', permission);
         if (permission === 'granted') {
           console.log('알림 권한 허용됨');
           await retrieveToken(firebaseRegistration);
@@ -78,8 +46,8 @@ async function registerServiceWorker() {
 
 async function retrieveToken(serviceWorkerRegistration) {
   try {
-    const currentToken = await getToken(messaging, {
-      vapidKey: FIREBASE_VAPID_KEY,
+    const currentToken = await messaging.getToken({
+      vapidKey: 'BHwpLNL8I7G3uVXMnYxMZWiopgbTh5k0bC-HRQPEVXj6mz7cPrjcNpgEhwDf08bq99wJF7CQA3DRa16Zb5z2Tvs',
       serviceWorkerRegistration: serviceWorkerRegistration,
     });
 
