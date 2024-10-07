8f796356a60de4d321aabfca03cf1588e197ac9b
인호현 <inhohyun36@gmail.com>
2024-10-08 00:02:08 +0900

    fix: 식습관 매핑 앞뒤 공백 제거

--- project/front/health/src/api/survey.ts
+++ project/front/health/src/api/survey.ts
@@ -8,7 +8,6 @@ export const nicknameCheck = async (nickname: string) => {
 };
 
 export const surveySubmit1 = async (nickname: string, birthday: string, send_gender: string) => {
-  // console.log(nickname, birthday, gender);
   const gender = send_gender === '남성' ? 'MAN' : 'WOMAN';
   const response = await exportAxios.post(`${baseUrl}/users/survey/information`, {
     nickname,
@@ -27,10 +26,9 @@ export const surveySubmit2 = async (
   isMuscle: boolean,
   bodyTypeString: string
 ) => {
-  // console.log(height, weight, skeletalMuscleMass, bodyFatRatio, isMuscle, bodyType);
   let bodyTypeNumber: number;
 
-  switch (bodyTypeString) {
+  switch (bodyTypeString.trim()) {
     case '슬림':
       bodyTypeNumber = 1;
       break;
@@ -52,7 +50,7 @@ export const surveySubmit2 = async (
     default:
       throw new Error('Invalid body type');
   }
-  console.log(height, weight, skeletalMuscleMass, bodyFatRatio, isMuscle, bodyTypeNumber);
+
   const response = await exportAxios.post(`${baseUrl}/users/survey/body`, {
     height,
     weight,
@@ -74,8 +72,7 @@ export const surveySubmit3 = async (
   console.log('surveySubmit3', pre_mealCount, pre_dietType, pre_snackFrequency, pre_drinkFrequency);
 
   let mealCount: number;
-
-  switch (pre_mealCount) {
+  switch (pre_mealCount.trim()) {
     case '1끼':
       mealCount = 1;
       break;
@@ -92,8 +89,9 @@ export const surveySubmit3 = async (
       mealCount = 1;
       console.log('pre_mealCount', pre_mealCount);
   }
+
   let dietType: string;
-  switch (pre_dietType) {
+  switch (pre_dietType.trim()) {
     case '주로 채식 (채소, 과일 중심)':
       dietType = 'VEGETARIAN';
       break;
@@ -110,8 +108,9 @@ export const surveySubmit3 = async (
       console.log('pre_dietType', pre_dietType);
       dietType = 'VEGETARIAN';
   }
-  let snackFrequency;
-  switch (pre_snackFrequency) {
+
+  let snackFrequency: string;
+  switch (pre_snackFrequency.trim()) {
     case '전혀 섭취하지 않음':
       snackFrequency = 'NEVER';
       break;
@@ -129,8 +128,8 @@ export const surveySubmit3 = async (
       snackFrequency = 'NEVER';
   }
 
-  let drinkFrequency;
-  switch (pre_drinkFrequency) {
+  let drinkFrequency: string;
+  switch (pre_drinkFrequency.trim()) {
     case '전혀 섭취하지 않음':
       drinkFrequency = 'NEVER';
       break;
@@ -147,7 +146,9 @@ export const surveySubmit3 = async (
       console.log('pre_drinkFrequency', pre_drinkFrequency);
       drinkFrequency = 'NEVER';
   }
+
   console.log('식습관 설문 보낼 데이터', mealCount, dietType, snackFrequency, drinkFrequency);
+
   const response = await exportAxios.post(`${baseUrl}/users/survey/eating-habits`, {
     mealCount,
     mealType: dietType,
