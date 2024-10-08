06af19620030c600b2595960f774357d4c627777
송준혁 <fprhqkrtk303@naver.com>
2024-10-08 21:16:37 +0900

    test: 기본 체형 예측 요청 테스트 API

--- project/back/health/src/main/java/com/ssafy/health/domain/body/BodyPredict/controller/BodyPredictController.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/body/BodyPredict/controller/BodyPredictController.java
@@ -37,7 +37,7 @@ public class BodyPredictController implements BodyPredictControllerApi {
     }
 
     @PostMapping("/request-analysis")
-    public ApiResponse<AnalysisRequestDto> requestAnalysis() throws JsonProcessingException {
-        return ApiResponse.success(bodyPredictWriteService.requestPrediction(), "FastAPI Request");
+    public ApiResponse<Void> requestAnalysis() {
+        return ApiResponse.success(bodyPredictWriteService.requestPrediction(), "FastAPI Basic Request");
     }
 }
