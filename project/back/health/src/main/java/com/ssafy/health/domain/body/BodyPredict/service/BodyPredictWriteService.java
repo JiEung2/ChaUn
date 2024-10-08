9f021072375333f3de92e246627c5266dbbe1c76
송준혁 <fprhqkrtk303@naver.com>
2024-10-08 21:16:11 +0900

    refactor: 체형 예측 조회 메서드 수정
    - 기본 체형 예측은 모든 유저를 대상으로 순회하도록 변경
    - FastAPI URL은 유저 ID를 입력받도록 변경
    - 기록 개수 조정 TODO 삭제

--- project/back/health/src/main/java/com/ssafy/health/domain/body/BodyPredict/service/BodyPredictWriteService.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/body/BodyPredict/service/BodyPredictWriteService.java
@@ -3,7 +3,6 @@ package com.ssafy.health.domain.body.BodyPredict.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ssafy.health.common.security.SecurityUtil;
 import com.ssafy.health.common.util.RequestUtil;
-import com.ssafy.health.common.util.WeeklyRequestDto;
 import com.ssafy.health.domain.account.entity.Gender;
 import com.ssafy.health.domain.account.entity.User;
 import com.ssafy.health.domain.account.exception.UserNotFoundException;
@@ -22,14 +21,13 @@ import com.ssafy.health.domain.exercise.service.ExerciseHistoryWriteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.temporal.WeekFields;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
@@ -45,51 +43,59 @@ public class BodyPredictWriteService {
     private final ExerciseRepository exerciseRepository;
     private final RequestUtil requestUtil;
 
-    private String apiBaseUrlBuilder() {
-        Long userId = SecurityUtil.getCurrentUserId();
-        return fastApiUrl + "/users/" + userId.toString() + "/body/prediction";
+    private String apiBaseUrlBuilder(Long userId) {
+        return fastApiUrl + "/users/" + userId + "/body/prediction";
     }
 
-    public AnalysisRequestDto requestPrediction() throws JsonProcessingException {
+    @Scheduled(cron = "0 30 4 * * *")
+    public Void requestPrediction() {
 
-        // TODO: 스프링 스케쥴러 이용, FastAPI에 분석 요청 보내기
-        String apiUrl = apiBaseUrlBuilder() + "/fast-api";
+        List<User> userList = userRepository.findAll();
 
-        ExerciseDetailDto dto = ExerciseDetailDto.builder().build();
-        AnalysisRequestDto requestDto = buildPredictionPayload(PredictionType.BASIC, dto);
-        requestUtil.sendPostRequest(apiUrl, requestDto, String.class);
-        return buildPredictionPayload(PredictionType.BASIC, dto);
+        userList.forEach(user -> {
+            String apiUrl = apiBaseUrlBuilder(user.getId()) + "/fast-api";
+
+            ExerciseDetailDto dto = ExerciseDetailDto.builder().build();
+            AnalysisRequestDto requestDto = buildPredictionPayload(user.getId(), PredictionType.BASIC, dto);
+
+            try {
+                requestUtil.sendPostRequest(apiUrl, requestDto, String.class);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return null;
     }
 
     public AnalysisRequestDto requestExtraAnalysis(ExerciseDetailDto exerciseDetail) throws JsonProcessingException {
 
-        // TODO: API 요청 성공 유무에 따른 반환값 추가
-        String apiUrl = apiBaseUrlBuilder() + "/extra/fast-api";
+        Long userId = SecurityUtil.getCurrentUserId();
+        String apiUrl = apiBaseUrlBuilder(userId) + "/extra/fast-api";
 
-        AnalysisRequestDto dto = buildPredictionPayload(PredictionType.EXTRA, exerciseDetail);
+        AnalysisRequestDto dto = buildPredictionPayload(userId, PredictionType.EXTRA, exerciseDetail);
         ResponseEntity<String> response = requestUtil.sendPostRequest(apiUrl, dto, String.class);
         return dto;
     }
 
     private AnalysisRequestDto buildPredictionPayload(
-            PredictionType predictionType, ExerciseDetailDto exerciseDetail) {
+            Long userId, PredictionType predictionType, ExerciseDetailDto exerciseDetail) {
 
-        User user = findUserById(SecurityUtil.getCurrentUserId());
-
-        LocalDate today = LocalDate.now();
-        int weekNumber = today.get(WeekFields.of(Locale.KOREA).weekOfMonth());
-        WeeklyRequestDto dateDto = new WeeklyRequestDto(today.getYear(), today.getMonthValue(), weekNumber - 1);
-        ExerciseHistoryListResponseDto exerciseHistory = exerciseReadService.getWeeklyExerciseHistory(dateDto);
+        User user = findUserById(userId);
+        ExerciseHistoryListResponseDto exerciseHistory = exerciseReadService.getLastWeekExerciseHistory(userId);
 
         BodyHistory bodyHistory = bodyRepository.findFirstByUserIdOrderByCreatedAtDesc(user.getId())
                 .orElseThrow(BodyHistoryNotFoundException::new);
 
-        // TODO: 조회된 운동 기록이 7개 이상일 경우 7개까지만 반환
-        // TODO: 추가 예측 요청일 경우 3개까지만 반환
         List<AnalysisRequestDto.UserExerciseData> exerciseBasicList = exerciseHistory.getExerciseHistoryList()
                 .stream()
                 .map(exercise -> exerciseDataBuilder(user, bodyHistory, exercise.getBurnedCalories()))
                 .toList();
+
+        if (predictionType.equals(PredictionType.EXTRA) && exerciseBasicList.size() > 3) {
+            exerciseBasicList = exerciseBasicList.subList(0, 3);
+        } else if (predictionType.equals(PredictionType.BASIC) && exerciseBasicList.size() > 7) {
+            exerciseBasicList = exerciseBasicList.subList(0, 7);
+        }
 
         return AnalysisRequestDto.builder()
                 .exerciseDetail(
@@ -97,8 +103,7 @@ public class BodyPredictWriteService {
                 .exerciseData(exerciseBasicList)
                 .extraExerciseData(
                         (predictionType.equals(PredictionType.EXTRA)) ?
-                                buildExtraPayload(user, bodyHistory, exerciseDetail) : null
-                )
+                                buildExtraPayload(user, bodyHistory, exerciseDetail) : null)
                 .build();
     }
 
