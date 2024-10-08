8822fc277a2c3ef2473e000e5d2fc616a660685c
송준혁 <fprhqkrtk303@naver.com>
2024-10-08 21:08:52 +0900

    feat: 직전 주의 운동 기록 조회 메서드

--- project/back/health/src/main/java/com/ssafy/health/domain/exercise/service/ExerciseHistoryReadService.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/exercise/service/ExerciseHistoryReadService.java
@@ -1,8 +1,5 @@
 package com.ssafy.health.domain.exercise.service;
 
-import static java.time.temporal.TemporalAdjusters.firstDayOfMonth;
-import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
-
 import com.ssafy.health.common.security.SecurityUtil;
 import com.ssafy.health.common.util.MonthlyRequestDto;
 import com.ssafy.health.common.util.WeeklyRequestDto;
@@ -10,16 +7,19 @@ import com.ssafy.health.domain.exercise.dto.response.ExerciseHistoryListResponse
 import com.ssafy.health.domain.exercise.dto.response.WeeklyAndDailyExerciseTimeResponseDto;
 import com.ssafy.health.domain.exercise.entity.ExerciseHistory;
 import com.ssafy.health.domain.exercise.repository.ExerciseHistoryRepository;
-
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.temporal.TemporalAdjusters;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.*;
+import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.WeekFields;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+import static java.time.temporal.TemporalAdjusters.firstDayOfMonth;
+import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
 
 @Service
 @Transactional(readOnly = true)
@@ -54,6 +54,24 @@ public class ExerciseHistoryReadService {
         return getExerciseHistoryListResponseDto(dateTimes);
     }
 
+    public ExerciseHistoryListResponseDto getLastWeekExerciseHistory(Long userId) {
+
+        LocalDate today = LocalDate.now(ZoneId.of("Asia/Seoul"));
+
+        int weekYearNumber = today.get(WeekFields.of(Locale.KOREA).weekOfYear()) - 1;
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+        calendar.set(Calendar.WEEK_OF_YEAR, weekYearNumber);
+        calendar.set(Calendar.YEAR, today.getYear());
+
+        LocalDateTime startDateTime = calendar.getTime().toInstant()
+                .atZone(ZoneId.of("Asia/Seoul"))
+                .toLocalDateTime();
+        LocalDateTime endDateTime = startDateTime.plusWeeks(1).minusSeconds(1);
+        return getExerciseHistoryListResponseDto(userId, new LocalDateTime[]{startDateTime, endDateTime});
+    }
+
     public ExerciseHistoryListResponseDto getMonthlyExerciseHistory(MonthlyRequestDto requestDto) {
         LocalDateTime[] dateTimes = calculateMonthDateTimeRange(requestDto.getYear(), requestDto.getMonth());
         return getExerciseHistoryListResponseDto(dateTimes);
@@ -72,11 +90,26 @@ public class ExerciseHistoryReadService {
                 .build();
     }
 
+    private ExerciseHistoryListResponseDto getExerciseHistoryListResponseDto(Long userId, LocalDateTime[] dateTimes) {
+        LocalDateTime startDateTime = dateTimes[0];
+        LocalDateTime endDateTime = dateTimes[1];
+
+        List<ExerciseHistory> exerciseHistoryList = exerciseHistoryRepository.
+                findByUserIdAndCreatedAtBetween(userId, startDateTime, endDateTime);
+
+        List<ExerciseHistoryListResponseDto.ExerciseHistoryDetailDto> exerciseHistoryDetailList =
+                getExerciseHistoryDetailDtoList(exerciseHistoryList);
+
+        return ExerciseHistoryListResponseDto.builder()
+                .exerciseHistoryList(exerciseHistoryDetailList)
+                .build();
+    }
+
     private Long getTotalExerciseTime(final Long userId) {
         return calculateExerciseTime(exerciseHistoryRepository.findByUserId(userId));
     }
 
-    private Long getDailyAccumulatedExerciseTime (final Long userId) {
+    private Long getDailyAccumulatedExerciseTime(final Long userId) {
         LocalDateTime today = LocalDateTime.now();
         LocalDateTime startTime = today.toLocalDate().atStartOfDay();
         LocalDateTime endTime = today.toLocalDate().atTime(23, 59, 59);
@@ -94,7 +127,7 @@ public class ExerciseHistoryReadService {
                 userId, startTime, endTime));
     }
 
-    private Long getMonthlyAccumulatedExerciseTime (final Long userId) {
+    private Long getMonthlyAccumulatedExerciseTime(final Long userId) {
         LocalDateTime today = LocalDateTime.now();
         LocalDateTime startTime = today.with(firstDayOfMonth()).with(LocalTime.MIN);
         LocalDateTime endTime = today.with(lastDayOfMonth()).with(LocalTime.MAX);
@@ -110,7 +143,7 @@ public class ExerciseHistoryReadService {
                 .sum();
     }
 
-    private LocalDateTime[] calculateWeekDateTimeRange(int year, int month, int week){
+    private LocalDateTime[] calculateWeekDateTimeRange(int year, int month, int week) {
         LocalDate firstDayOfWeek = LocalDate.of(year, month, 1);
         LocalDate firstMondayOfMonth = firstDayOfWeek.with(TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY));
         LocalDate startOfWeek = firstMondayOfMonth.plusWeeks(week - 1);
@@ -121,7 +154,7 @@ public class ExerciseHistoryReadService {
         return new LocalDateTime[]{startDateTime, endDateTime};
     }
 
-    private LocalDateTime[] calculateMonthDateTimeRange(int year, int month){
+    private LocalDateTime[] calculateMonthDateTimeRange(int year, int month) {
         LocalDate startOfMonth = LocalDate.of(year, month, 1);
         LocalDate endOfMonth = startOfMonth.with(TemporalAdjusters.lastDayOfMonth());
 
