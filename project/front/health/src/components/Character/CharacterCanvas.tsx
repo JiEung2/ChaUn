277143bff839db2e0878b8a6aecf58d1f4117869
minyeong981 <rlaalsdud41@naver.com>
2024-10-09 23:32:32 +0900

    test: 여자 캐릭터 20

--- project/front/health/src/components/Character/CharacterCanvas.tsx
+++ project/front/health/src/components/Character/CharacterCanvas.tsx
@@ -66,7 +66,7 @@ function Character({ glbUrl, gender, setLoading }: CharacterProps & { setLoading
           model.scale.set(1.1, 1.1, 1.1);
           model.position.set(0, -8.5, 0);
         } else if (gender === 'WOMAN') {
-          model.scale.set(10, 10, 10);
+          model.scale.set(20, 20, 20);
           model.position.set(0, -8.5, 0);
         }
 
