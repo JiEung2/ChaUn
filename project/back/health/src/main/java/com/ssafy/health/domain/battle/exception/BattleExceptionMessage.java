df556e3acff30c30cc096e5636d2b1a0b562a391
JiEung2 <181757@jnu.ac.kr>
2024-10-09 21:15:20 +0900

    feat: 해당 배틀에 참여중인 크루가 아닐 때의 예외 메시지 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/battle/exception/BattleExceptionMessage.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/battle/exception/BattleExceptionMessage.java
@@ -8,6 +8,7 @@ public enum BattleExceptionMessage {
 
     BATTLE_NOT_FOUND("현재 진행중인 배틀이 존재하지 않습니다.", HttpStatus.NOT_FOUND.value()),
     BATTLE_ALREADY_EXISTS("이미 진행중인 배틀이 존재합니다.", HttpStatus.CONFLICT.value()),
+    CREW_NOT_PARTICIPANT("해당 배틀에 참여중인 크루가 아닙니다.", HttpStatus.BAD_REQUEST.value())
     ;
 
     private final String message;
