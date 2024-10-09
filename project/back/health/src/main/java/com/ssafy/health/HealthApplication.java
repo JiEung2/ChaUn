a8fdc05b4b8099c9208c38712a6db76ca2b41224
송준혁 <fprhqkrtk303@naver.com>
2024-10-09 16:28:32 +0900

    test: Timezone 확인을 위한 로그 추가

--- project/back/health/src/main/java/com/ssafy/health/HealthApplication.java
+++ project/back/health/src/main/java/com/ssafy/health/HealthApplication.java
@@ -1,27 +1,32 @@
 package com.ssafy.health;
 
 import jakarta.annotation.PostConstruct;
-import java.time.LocalDateTime;
-import java.util.TimeZone;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Date;
+import java.util.TimeZone;
+
+@Slf4j
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableMongoAuditing
 @EnableScheduling
 public class HealthApplication {
 
-	@PostConstruct
-	public void init() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        log.info("Current Timezone: {}", TimeZone.getDefault().getID());
+        log.info("Current Time: {}", new Date());
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(HealthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HealthApplication.class, args);
+    }
 
 }
