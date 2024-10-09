699d7c36a44c2aa7b9c6988fa7ed76bf5c7cd719
minyeong981 <rlaalsdud41@naver.com>
2024-10-10 08:50:07 +0900

    design: 캐릭터 크기 조절

--- project/front/health/src/components/Character/CharacterCanvas.tsx
+++ project/front/health/src/components/Character/CharacterCanvas.tsx
@@ -63,8 +63,8 @@ function Character({ glbUrl, gender, setLoading }: CharacterProps & { setLoading
 
         // 캐릭터의 스케일과 위치 설정
         if (gender === 'MAN') {
-          model.scale.set(1.1, 1.1, 1.1);
-          model.position.set(0, -10, 0);
+          model.scale.set(1, 1, 1);
+          model.position.set(0, -9, 0);
         } else if (gender === 'WOMAN') {
           model.scale.set(10, 10, 10);
           model.position.set(0, -9, 0);
