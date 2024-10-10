e4c4e1abe6e3607d178a90c09d67833674ae0ccb
인호현 <inhohyun36@gmail.com>
2024-10-10 16:21:08 +0900

    fix: service worker url 수정

--- project/front/health/public/service-worker.js
+++ project/front/health/public/service-worker.js
@@ -99,8 +99,8 @@ self.addEventListener('fetch', (event) => {
   const requestUrl = new URL(event.request.url);
 
   if (
-    url.origin === `https://j11c106.p.ssafy.io/oauth2/authorization/google` ||
-    url.origin === `https://j11c106.p.ssafy.io/oauth2/authorization/kakao`
+    requestUrl.origin === `https://j11c106.p.ssafy.io/oauth2/authorization/google` ||
+    requestUrl.origin === `https://j11c106.p.ssafy.io/oauth2/authorization/kakao`
   ) {
     // 소셜 로그인과 관련된 요청은 캐싱하지 않고 네트워크로만 처리
     // return fetch(event.request);
