0ef91f784013aac22c7d8cc3426193e90d044128
JiEung2 <181757@jnu.ac.kr>
2024-10-10 20:23:51 +0900

    feat: CharacterSetRepository 쿼리 추가
    - user리스트로 character를 검색하는 쿼리 추가

--- project/back/health/src/main/java/com/ssafy/health/domain/character/respository/CharacterSetRepository.java
+++ project/back/health/src/main/java/com/ssafy/health/domain/character/respository/CharacterSetRepository.java
@@ -1,9 +1,14 @@
 package com.ssafy.health.domain.character.respository;
 
 import com.ssafy.health.domain.character.entity.CharacterSet;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CharacterSetRepository extends JpaRepository<CharacterSet, Long> {
     Optional<CharacterSet> findByUserId(Long userId);
+
+    @Query("SELECT cs.user.id, cs.character FROM CharacterSet cs WHERE cs.user.id IN :crewMemberIdList")
+    List<Object[]> findCharacterByUserIdIn(List<Long> crewMemberIdList);
 }
