5071baf14daa28b168eeb2e9f239bf77bed59f17
인호현 <inhohyun36@gmail.com>
2024-10-10 21:01:11 +0900

    fix: 크루 추천 리스트 코드 수정

--- project/front/health/src/pages/Crew/CrewRecommend/CrewRecommend.tsx
+++ project/front/health/src/pages/Crew/CrewRecommend/CrewRecommend.tsx
@@ -5,24 +5,34 @@ import CrewModal from '../../../components/Crew/CrewModal';
 import { getCrewRecommendList } from '@/api/crew';
 import useUserStore from '@/store/userInfo';
 
-interface Crew {
+interface CrewDetail {
   crewId: number;
   crewProfileImage: string;
-  crewName: string;
+  name: string;
   exerciseName: string;
+  description: string;
 }
 
 export default function CrewRecommend() {
   const [nickname] = useState(useUserStore.getState().nickname);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedCrewId, setSelectedCrewId] = useState<number | null>(null);
-  const [crews, setCrews] = useState<Crew[]>([]); // crews 배열을 상태로 관리
-
+  const [crews, setCrews] = useState<CrewDetail[]>([]); // CrewDetail[]로 타입 지정
   const getCrewRecommendListData = async () => {
     try {
       const response = await getCrewRecommendList();
       console.log('크루 추천 페이지에서 response', response);
-      setCrews(response.crewRecommend); // API 호출 후 데이터를 상태에 저장
+
+      // 각 크루의 세부 정보를 포함하는 배열로 crews 상태를 업데이트
+      const mappedCrews = response.crewRecommend.map((crew: any) => ({
+        crewId: crew.crewId,
+        crewProfileImage: crew.crewDetail.profileImage,
+        name: crew.crewDetail.name,
+        exerciseName: crew.crewDetail.exerciseName,
+        description: crew.crewDetail.description,
+      }));
+
+      setCrews(mappedCrews); // 상태 업데이트
     } catch (error) {
       console.error('크루추천 리스트 불러오기 실패', error);
     }
@@ -52,9 +62,9 @@ export default function CrewRecommend() {
         <div className="crew-grid">
           {crews?.map((crew) => (
             <Crew
-              key={crew.crewId}
+              key={crew.crewId} // crewId로 key 설정
               imageUrl={crew.crewProfileImage}
-              name={crew.crewName}
+              name={crew.name}
               tag={crew.exerciseName}
               onClick={() => handleCrewClick(crew.crewId)}
             />
